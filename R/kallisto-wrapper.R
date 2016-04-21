5b5f451402d3df65a1528ed29bd37366bcee64d5
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-04-21 18:33:23 +0000

    Merge branch 'master' into devel
    From: Dan Tenenbaum <dtenenba@fhcrc.org>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/scater@116666 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/kallisto-wrapper.R
+++ R/kallisto-wrapper.R
@@ -24,7 +24,11 @@
 #' @param fragment_length scalar integer or numeric giving the estimated
 #' average fragment length. Required argument if \code{single_end} is \code{TRUE},
 #' optional if \code{FALSE} (kallisto default for paired-end data is that the
-#' value is estimated from the input data.
+#' value is estimated from the input data).
+#' @param fragment_standard_deviation scalar numeric giving the estimated 
+#' standard deviation of read fragment length. Required argument if 
+#' \code{single_end} is \code{TRUE}, optional if \code{FALSE} (kallisto default 
+#' for paired-end data is that the value is estimated from the input data).
 #' @param n_cores integer giving the number of cores (nodes/threads) to use for
 #' the kallisto jobs. The package \code{parallel} is used. Default is 2 cores.
 #' @param n_bootstrap_samples integer giving the number of bootstrap samples
@@ -79,6 +83,7 @@
 #' }
 runKallisto <- function(targets_file, transcript_index, single_end = TRUE,
                         output_prefix = "output", fragment_length = NULL,
+                        fragment_standard_deviation = NULL,
                         n_cores = 2, n_bootstrap_samples = 0,
                         bootstrap_seed = NULL, correct_bias = TRUE,
                         plaintext = FALSE, kallisto_version = "current",
@@ -108,6 +113,10 @@ defined. Either a scalar giving the average fragment length to use for all
 samples, or a vector providing the ave fragment length for each sample.")
     else
         paired_end <- !single_end
+    if ( single_end && is.null(fragment_standard_deviation) )
+        stop("If single-end reads are used, then fragment_standard_deviation must be defined.")
+    else
+        paired_end <- !single_end
     if ( correct_bias && kallisto_version == "pre-0.42.2" )
         stop("Bias correction requires kallisto version 0.42.2 or higher.")
     ## Make sure that we'll be able to find the fastq files
@@ -123,6 +132,8 @@ samples, or a vector providing the ave fragment length for each sample.")
         kallisto_args <- paste(kallisto_args, "--single")
     if ( !is.null(fragment_length) )
         kallisto_args <- paste(kallisto_args, "-l", fragment_length)
+    if ( !is.null(fragment_standard_deviation) )
+        kallisto_args <- paste(kallisto_args, "-s", fragment_standard_deviation)
     if ( !is.null(bootstrap_seed) )
         kallisto_args <- paste0(kallisto_args, " --seed=", bootstrap_seed)
     if ( correct_bias && kallisto_version != "pre-0.42.2" )
@@ -226,9 +237,11 @@ readKallistoResultsOneSample <- function(directory, read_h5=FALSE,
         file_to_read <- paste0(directory, "/abundance.txt")
     else
         file_to_read <- paste0(directory, "/abundance.tsv")
-    if ( file.exists(file_to_read) )
-        abundance <- data.table::fread(file_to_read, sep = "\t")
-    else
+    if ( file.exists(file_to_read) ) {
+        abundance <- data.table::fread(file_to_read, colClasses = c("numeric", "numeric", "numeric", "character", "character"), sep = "\t")
+        abundance$est_counts <- as.numeric(abundance$est_counts)
+        abundance$tpm <- as.numeric(abundance$tpm)
+    } else
         stop(paste("File", file_to_read, "not found or does not exist. Please check directory is correct."))
     ## Read in run information
     run_info <- rjson::fromJSON(file = paste0(directory, "/run_info.json"))
--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -9,7 +9,7 @@ test_that("we can produce PCA plots with different expression values", {
     example_sceset <- newSCESet(countData = sc_example_counts, phenoData = pd)
     example_sceset <- calculateQCMetrics(example_sceset)
 
-    expect_output(class(plotPCA(example_sceset)), "ggplot")
+    expect_that(plotPCA(example_sceset), is_a("ggplot"))
 })
 
 test_that("we can produce t-SNE plots with different expression values", {
@@ -19,7 +19,7 @@ test_that("we can produce t-SNE plots with different expression values", {
     example_sceset <- newSCESet(countData = sc_example_counts, phenoData = pd)
     example_sceset <- calculateQCMetrics(example_sceset)
 
-    expect_output(class(plotTSNE(example_sceset)), "ggplot")
+    expect_that(plotTSNE(example_sceset), is_a("ggplot"))
 })
 
 test_that("we can produce Diffusion Map plots with different expression values",
@@ -31,7 +31,7 @@ test_that("we can produce Diffusion Map plots with different expression values",
                                           phenoData = pd)
               example_sceset <- calculateQCMetrics(example_sceset)
 
-              expect_output(class(plotDiffusionMap(example_sceset)), "ggplot")
+              expect_that(plotDiffusionMap(example_sceset), is_a("ggplot"))
           })
 
 test_that("we can produce MDS plots with different expression values",
@@ -45,12 +45,12 @@ test_that("we can produce MDS plots with different expression values",
               cellDist(example_sceset) <- as.matrix(
                   dist(t(exprs(example_sceset))))
 
-              expect_output(class(plotMDS(example_sceset)), "ggplot")
-              expect_output(class(plotMDS(example_sceset,
+              expect_that(plotMDS(example_sceset), is_a("ggplot"))
+              expect_that(plotMDS(example_sceset,
                                           colour_by = "Cell_Cycle",
                                           shape_by = "Treatment",
-                                          size_by = "Mutation_Status")),
-                            "ggplot")
+                                          size_by = "Mutation_Status"),
+                            is_a("ggplot"))
           })
 
 context("test plotExpression")
@@ -62,7 +62,7 @@ test_that("we can produce expression plots with different expression values", {
     example_sceset <- newSCESet(countData = sc_example_counts, phenoData = pd)
     example_sceset <- calculateQCMetrics(example_sceset)
 
-    expect_output(class(plotExpression(example_sceset, 1:4, "Cell_Cycle")), "ggplot")
-    expect_output(class(plotExpression(example_sceset, 1:4, "Gene_0004")), "ggplot")
+    expect_that(plotExpression(example_sceset, 1:4, "Cell_Cycle"), is_a("ggplot"))
+    expect_that(plotExpression(example_sceset, 1:4, "Gene_0004"), is_a("ggplot"))
 })
 
