0d07b4c376e97b98c24d2a73add86ebd8df6863f
Aaron Lun <alun@wehi.edu.au>
2018-01-18 18:00:53 +0000

    Removed old SCESet methods.

--- tests/testthat/test-SCESet-methods.R
+++ tests/testthat/test-SCESet-methods.R
@@ -1,14 +1,6 @@
 ## Testing SCESet methods
 
 test_that("newSCESet fails as expected", {
-    data("sc_example_counts")
-    data("sc_example_cell_info")
-    pd <- new("AnnotatedDataFrame", data = sc_example_cell_info)
-    expect_warning((example_sceset <- 
-                       newSCESet(countData = sc_example_counts, phenoData = pd)),
-                   "deprecated")
-    expect_that(example_sceset, is_a("SingleCellExperiment"))
-    
     sceset <- readRDS(system.file("extdata", "test_sceset.rds", package = "scater"))
     expect_that(toSingleCellExperiment(sceset), is_a("SingleCellExperiment"))
     expect_that(updateSCESet(sceset), is_a("SingleCellExperiment"))
