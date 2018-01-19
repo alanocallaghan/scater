d7988ac627843cc068c56cf171dd0f4f3c16aad6
Aaron Lun <alun@wehi.edu.au>
2018-01-19 10:24:25 +0000

    Added uniquifyFeatureNames for quick uniquification.

--- tests/testthat/test-feature-preprocessing.R
+++ tests/testthat/test-feature-preprocessing.R
@@ -49,3 +49,16 @@ test_that("we can summarise expression at feature level", {
                  "lib_size argument must have length equal")
 
 })
+
+test_that("we can uniquify the feature names", {
+    all.genes <- sample(c(LETTERS, LETTERS[1:5], NA, NA))
+    all.ids <- paste0("GENE", seq_along(all.genes))
+    out <- uniquifyFeatureNames(all.ids, all.genes)
+
+    lost <- is.na(all.genes)
+    expect_identical(out[lost], all.ids[lost])
+    dup <- all.genes %in% all.genes[duplicated(all.genes)]
+    expect_identical(out[!dup & !lost], all.genes[!dup & !lost])
+    expect_identical(out[dup & !lost], paste0(all.genes, "_", all.ids)[dup & !lost])
+})
+
