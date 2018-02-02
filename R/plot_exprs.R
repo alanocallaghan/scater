9568bea21b31d12693f465cd227222865c7f1a00
Aaron Lun <alun@wehi.edu.au>
2018-02-02 09:50:06 +0000

    Moved plot-expression functions to separate files.

---
+++ R/plot_exprs.R
@@ -0,0 +1,297 @@
+#' Plot expression values for a set of features (e.g. genes or transcripts)
+#'
+#' @param object A SingleCellExperiment object containing expression values and
+#' experimental information. 
+#' @param features a character vector of feature names or Boolean
+#' vector or numeric vector of indices indicating which features should have
+#' their expression values plotted
+#' @param x character string providing a column name of \code{pData(object)} or
+#' a feature name (i.e. gene or transcript) to plot on the x-axis in the
+#' expression plot(s). If a feature name, then expression values for the feature
+#' will be plotted on the x-axis for each subplot.
+#' @param exprs_values character string indicating which values should be used
+#' as the expression values for this plot. Valid arguments are \code{"tpm"}
+#' (transcripts per million), \code{"norm_tpm"} (normalised TPM
+#' values), \code{"fpkm"} (FPKM values), \code{"norm_fpkm"} (normalised FPKM
+#' values), \code{"counts"} (counts for each feature), \code{"norm_counts"},
+#' \code{"cpm"} (counts-per-million), \code{"norm_cpm"} (normalised
+#' counts-per-million), \code{"logcounts"} (log-transformed count data; default),
+#' \code{"norm_exprs"} (normalised
+#' expression values) or \code{"stand_exprs"} (standardised expression values)
+#' or any other slots that have been added to the \code{"assayData"} slot by
+#' the user.
+#' @param colour_by optional character string supplying name of a column of
+#' \code{pData(object)} which will be used as a variable by which to colour
+#' expression values on the plot. Alternatively, a data frame with one column,
+#' containing a value for each cell that will be mapped to a colour.
+#' @param shape_by optional character string supplying name of a column of
+#' \code{pData(object)} which will be used as a variable to define the shape of
+#' points for expression values on the plot. Alternatively, a data frame with
+#' one column containing values to map to shapes.
+#' @param size_by optional character string supplying name of a column of
+#' \code{pData(object)} which will be used as a variable to define the size of
+#' points for expression values on the plot. Alternatively, a data frame with
+#' one column containing values to map to sizes.
+#' @param ncol number of columns to be used for the panels of the plot
+#' @param xlab label for x-axis; if \code{NULL} (default), then \code{x} will be
+#' used as the x-axis label
+#' @param show_median logical, show the median for each group on the plot
+#' @param show_violin logical, show a violin plot for the distribution
+#' for each group on the plot
+#' @param show_smooth logical, show a smoothed fit through the expression values
+#'  on the plot
+#' @param alpha numeric value between 0 (completely transparent) and 1 (completely
+#' solid) defining how transparent plotted points (cells) should be.
+#' Points are jittered horizontally if the x-axis value is categorical rather
+#' than numeric to avoid overplotting.
+#' @param theme_size numeric scalar giving default font size for plotting theme
+#' (default is 10)
+#' @param log2_values should the expression values be transformed to the
+#' log2-scale for plotting (with an offset of 1 to avoid logging zeroes)?
+#' @param size numeric scalar optionally providing size for points if
+#' \code{size_by} argument is not given. Default is \code{NULL}, in which case
+#' \pkg{ggplot2} default is used.
+#' @param scales character scalar, should scales be fixed ("fixed"),
+#' free ("free"), or free in one dimension ("free_x"; "free_y", the default).
+#' Passed to the \code{scales} argument in the \code{\link[ggplot2]{facet_wrap}} function
+#' from the \code{ggplot2} package.
+#' @param se logical, should standard errors be shown (default \code{TRUE}) for
+#' the smoothed fit through the cells. (Ignored if \code{show_smooth} is \code{FALSE}).
+#' @param jitter character scalar to define whether points are to be jittered
+#' (\code{"jitter"}) or presented in a "beeswarm" style (if \code{"swarm"}; default).
+#' "Beeswarm" style usually looks more attractive, but for datasets with a large
+#' number of cells, or for dense plots, the jitter option may work better.
+#'
+#' @details Plot expression values (default log2(counts-per-million +
+#' 1), if available) for a set of features.
+#'
+#' @return a ggplot plot object
+#'
+#' @name plotExpression
+#' @aliases plotExpression
+#' @import ggplot2
+#' @importFrom ggbeeswarm geom_quasirandom
+#' @export
+#'
+#' @examples
+#' ## prepare data
+#' data("sc_example_counts")
+#' data("sc_example_cell_info")
+#' example_sce <- SingleCellExperiment(
+#'     assays = list(counts = sc_example_counts), 
+#'     colData = sc_example_cell_info
+#' )
+#  example_sce <- normalize(example_sce)
+#' example_sce <- calculateQCMetrics(example_sce)
+#' sizeFactors(example_sce) <- colSums(counts(example_sce))
+#' example_sce <- normalize(example_sce)
+#'
+#' ## default plot
+#' plotExpression(example_sce, 1:15)
+#' plotExpression(example_sce, 1:15, jitter = "jitter")
+#'
+#' ## plot expression against an x-axis value
+#' plotExpression(example_sce, 1:6, "Mutation_Status")
+#'
+#' ## explore options
+#' plotExpression(example_sce, 1:6, x = "Mutation_Status", exprs_values = "logcounts",
+#'     colour_by = "Cell_Cycle", show_violin = TRUE, show_median = TRUE)
+#' plotExpression(example_sce, 1:6, x = "Mutation_Status", exprs_values = "counts",
+#'     colour_by = "Cell_Cycle", show_violin = TRUE, show_median = TRUE)
+#'
+#' plotExpression(example_sce, "Gene_0001", x = "Mutation_Status")
+#' plotExpression(example_sce, c("Gene_0001", "Gene_0004"), x="Mutation_Status")
+#' plotExpression(example_sce, "Gene_0001", x = "Gene_0002")
+#' plotExpression(example_sce, c("Gene_0001", "Gene_0004"), x="Gene_0002")
+#' ## plot expression against expression values for Gene_0004
+#' plotExpression(example_sce, 1:4, "Gene_0004")
+#' plotExpression(example_sce, 1:4, "Gene_0004", show_smooth = TRUE)
+#' plotExpression(example_sce, 1:4, "Gene_0004", show_smooth = TRUE, se = FALSE)
+#'
+plotExpression <- function(object, features, x = NULL,
+                           exprs_values = "logcounts", log2_values = FALSE,
+                           colour_by = NULL, shape_by = NULL, size_by = NULL,
+                           ncol = 2, xlab = NULL, 
+                           show_median = FALSE, show_violin = TRUE, show_smooth = FALSE,
+                           theme_size = 10, alpha = 0.6, size = NULL, scales = "fixed",
+                           one_facet = FALSE, se = TRUE, jitter = "swarm") 
+{
+    ## Define number of features to plot
+    if (is.logical(features)) {
+        nfeatures <- sum(features)
+    } else {
+        nfeatures <- length(features)
+    }
+
+    if ( exprs_values == "exprs" && !(exprs_values %in% assayNames(object)) ) {
+        exprs_values <- "logcounts"
+    }
+    exprs_mat <- assay(object, i = exprs_values)
+    exprs_mat <- exprs_mat[features,,drop = FALSE]
+    if ( log2_values ) {
+        exprs_mat <- log2(exprs_mat + 1)
+        ylab <- paste0("Expression (", exprs_values, "; log2-scale)")
+    } else {
+        ylab <- paste0("Expression (", exprs_values, ")")
+    }
+    to_melt <- as.matrix(exprs_mat)
+
+    ## check x-coordinates are valid
+    x_by_out <- .choose_vis_values(object, x, mode="column", search = "any", exprs_values = exprs_values)
+    xcoord <- x_by_out$val
+    if (is.null(xlab)) {
+        xlab <- x_by_out$name
+    }
+
+    ## checking visualization arguments
+    colour_by_out <- .choose_vis_values(object, colour_by, mode = "column", search = "any", 
+                                        exprs_values = exprs_values)
+    colour_by <- colour_by_out$name
+    colour_by_vals <- colour_by_out$val
+
+    shape_by_out <- .choose_vis_values(object, shape_by, mode = "column", search = "any", 
+                                       exprs_values = exprs_values, coerce_factor = TRUE, level_limit = 10)
+    shape_by <- shape_by_out$name
+    shape_by_vals <- shape_by_out$val
+
+    size_by_out <- .choose_vis_values(object, size_by, mode = "column", search = "any", 
+                                      exprs_values = exprs_values)
+    size_by <- size_by_out$name
+    size_by_vals <- size_by_out$val
+
+    ## Melt the expression data and metadata into a convenient form
+    evals_long <- reshape2::melt(to_melt, value.name = "evals")
+    colnames(evals_long) <- c("Feature", "Cell", "evals")
+
+    ## Prepare the samples information
+    samps <- data.frame(row.names = colnames(object))
+    if ( !is.null(xcoord) ) samps[[x]] <- xcoord
+
+    ## Construct a ggplot2 aesthetic for the plot
+    aesth <- aes()
+    if ( is.null(x) ) {
+        aesth$x <- as.symbol("Feature")
+        one_facet <- TRUE
+    } else {
+        aesth$x <- as.symbol(x)
+        one_facet <- FALSE
+    }
+    aesth$y <- as.symbol("evals")
+
+    if ( !is.null(shape_by) ) {
+        aesth$shape <- as.symbol(shape_by)
+        samps[[shape_by]] <- shape_by_vals
+    }
+    if ( !is.null(size_by) ) {
+        aesth$size <- as.symbol(size_by)
+        samps[[size_by]] <- size_by_vals
+    }
+    if ( !is.null(colour_by) ) {
+        aesth$colour <- as.symbol(colour_by)
+        samps[[colour_by]] <- colour_by_vals
+    }
+
+    ## Extend the sample information, combine with the expression values
+    samples_long <- samps[rep(seq_len(ncol(object)), each = nfeatures), , drop = FALSE]
+
+    ## create plotting object
+    object <- cbind(evals_long, samples_long)
+
+    ## Make the plot
+    plot_out <- plotExpressionDefault(object, aesth, ncol = ncol, xlab = xlab, ylab = ylab, 
+                                      show_median = show_median, show_violin = show_violin, show_smooth =show_smooth,
+                                      theme_size = theme_size, alpha = alpha, size = size, scales = scales,
+                                      one_facet = one_facet, se = se, jitter = jitter)
+
+    if ( is.null(x) ) { ## in this case, do not show x-axis ticks or labels
+        plot_out <- plot_out + theme(
+            axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1),
+            axis.ticks.x = element_blank(),
+            plot.margin = unit(c(.03, .02, .05, .02), "npc"))
+        if (is.null(colour_by)) {
+            plot_out <- plot_out + guides(fill = "none", colour = "none")
+        }
+    }
+    plot_out
+}
+
+plotExpressionDefault <- function(object, aesth, ncol = 2, xlab = NULL, ylab = NULL, 
+                                  show_median = FALSE, show_violin = TRUE, show_smooth = FALSE,
+                                  theme_size = 10, alpha = 0.6, size = NULL, scales = "fixed",
+                                  one_facet = FALSE, se = TRUE, jitter = "swarm") 
+{
+    x_groupable <- !is.numeric(object[[as.character(aesth$x)]])
+    if (x_groupable) {
+        show_smooth <- FALSE
+        aesth$group <- aesth$x
+    } else {
+        aesth$group <- 1
+        show_violin <- FALSE
+        show_median <- FALSE
+    }
+    
+    ## Define the plot
+    if (one_facet) {
+        if (is.null(aesth$colour))
+            aesth$colour <- as.symbol("Feature")
+        plot_out <- ggplot(object, aesth)
+    } else {
+        plot_out <- ggplot(object, aesth) +
+            facet_wrap(~Feature, ncol = ncol, scales = scales)
+    }
+    plot_out <- plot_out + xlab(xlab) + ylab(ylab)
+
+    ## if colour aesthetic is defined, then choose sensible colour palette
+    if ( !is.null(aesth$colour) ) {
+        plot_out <- .resolve_plot_colours(plot_out,
+                                          object[[as.character(aesth$colour)]],
+                                          as.character(aesth$colour))
+    }
+
+    ## if x axis variable is not numeric, then jitter points horizontally
+    if ( is.numeric(aesth$x) ) {
+        point_FUN <- geom_point
+    } else if (jitter=="swarm") {
+        point_FUN <- function(...) ggbeeswarm::geom_quasirandom(..., groupOnX=TRUE)
+    } else {
+        point_FUN <- function(...) geom_jitter(..., position = position_jitter(height = 0))
+    }
+
+    point_args <- list(alpha = alpha)
+    if ( is.null(aesth$size) & !is.null(size) ) {
+        point_args$size <- size
+    }
+    plot_out <- plot_out + do.call(point_FUN, point_args)
+
+    ## show optional decorations on plot if desired
+    if (show_violin) {
+        if (one_facet && (aesth$colour == as.symbol("Feature"))) {
+            plot_out <- plot_out +
+                geom_violin(aes_string(fill = "Feature"), colour = "gray60",
+                            alpha = 0.2, scale = "width")
+            plot_out <- .resolve_plot_colours(
+                plot_out, object[[as.character(aesth$colour)]],
+                as.character(aesth$colour), fill = TRUE)
+        } else {
+            plot_out <- plot_out + geom_violin(colour = "gray60", alpha = 0.3,
+                                               fill = "gray80", scale = "width")
+        }
+    }
+    if (show_median) {
+        plot_out <- plot_out +
+            stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,
+                         geom = "crossbar", width = 0.3, alpha = 0.8)
+    }
+    if (show_smooth) {
+        plot_out <- plot_out + stat_smooth(colour = "firebrick", linetype = 2, se = se)
+    }
+
+    ## Define plotting theme
+    if ( requireNamespace("cowplot", quietly = TRUE) ) {
+        plot_out <- plot_out + cowplot::theme_cowplot(theme_size)
+    } else {
+        plot_out <- plot_out + theme_bw(theme_size)
+    }
+
+    plot_out
+}
---
+++ R/plot_heat.R
@@ -0,0 +1,119 @@
+#' Plot heatmap of gene expression values
+#'
+#' @param object A \code{\link{SingleCellExperiment}} object.
+#' @param features A character vector containing row names of \code{object} 
+#' to be shown as rows in the heatmap.
+#' @param columns A vector specifying the subset of columns in \code{object}
+#' to show as columns in the heatmp. All columns are used by default.
+#' @param exprs_values A string indicating which assay of \code{object}
+#' should be used as expression values for colouring in the heatmap.
+#' @param center A logical scalar indicating whether each row should be 
+#' centered at zero prior to plotting. Setting this to \code{TRUE} 
+#' @param zlim A numeric vector of length 2, specifying the upper and lower
+#' bounds for the expression values. This winsorizes the expression matrix
+#' prior to plotting (but after centering, if \code{center=TRUE}. If 
+#' \code{NULL}, it defaults to the range of the expression matrix.
+#' @param symmetric A logical scalar specifying whether the default \code{zlim}
+#' should be symmetric around zero. If \code{TRUE}, the maximum absolute
+#' value of \code{zlim} will be computed and multiplied by \code{c(-1, 1)} to 
+#' redefine \code{zlim}.
+#' @param color A vector of colours specifying the palette to use for mapping 
+#' expression values to colours. This defaults to the default setting in 
+#' \code{\link[pheatmap]{pheatmap}}.
+#' @param colour_columns_by A character vector of column metadata names 
+#' in \code{colData(object)} or of gene names, to use to annotate the columns
+#' by colour.
+#' @param ... Additional arguments to pass to \code{\link[pheatmap]{pheatmap}}.
+#'
+#' @details Setting \code{center=TRUE} is useful for examining log-fold 
+#' changes of each cell's expression profile from the average across all cells.
+#' This avoids issues with the entire row appearing a certain colour because
+#' the gene is highly/lowly expressed across all cells.
+#'
+#' Setting \code{zlim} preserves the dynamic range of colours in the presence 
+#' of outliers. Otherwise, the plot may be dominated by a few genes,
+#' which will \dQuote{flatten} the observed colours for the rest of the heatmap.
+#'
+#' @return A heatmap is produced on the current graphics device. The output
+#' of \code{\link[pheatmap]{pheatmap}} is invisibly returned.
+#'
+#' @seealso \code{\link[pheatmap]{pheatmap}}
+#'
+#' @author Aaron Lun
+#' 
+#' @examples
+#' example(normalizeSCE) # borrowing the example objects in here.
+#' plotHeatmap(example_sce, features=rownames(example_sce)[1:10])
+
+#' plotHeatmap(example_sce, features=rownames(example_sce)[1:10],
+#'     center=TRUE, symmetric=TRUE)
+#'
+#' plotHeatmap(example_sce, features=rownames(example_sce)[1:10],
+#'     colour_columns_by=c("Mutation_Status", "Cell_Cycle"))
+#'
+#' @export
+plotHeatmap <- function(object, features, columns=NULL, exprs_values="logcounts",
+                        center=FALSE, zlim=NULL, symmetric=FALSE, color=NULL, 
+                        colour_columns_by=NULL, ...) {
+
+    heat.vals <- assay(object, exprs_values)[features,,drop=FALSE]
+    if (!is.null(columns)) { 
+        heat.vals <- heat.vals[,columns,drop=FALSE]        
+    }
+    if (center) {
+        heat.vals <- heat.vals - rowMeans(heat.vals)
+    }
+
+    # Winsorizing to preserve the dynamic range.
+    if (is.null(zlim)) {
+        zlim <- range(heat.vals)
+    }
+    if (symmetric) {
+        extreme <- max(abs(zlim))
+        zlim <- c(-extreme, extreme)
+    }
+    heat.vals[heat.vals < zlim[1]] <- zlim[1]
+    heat.vals[heat.vals > zlim[2]] <- zlim[2]
+
+    if (is.null(color)) {
+        color <- eval(formals(pheatmap::pheatmap)$color, envir=environment(pheatmap::pheatmap))
+    }
+    color.breaks <- seq(zlim[1], zlim[2], length.out=length(color)+1L) 
+
+    # Collecting variables to colour_by.
+    if (length(colour_columns_by)) {
+        column_variables <- column_colorings <- list()
+        for (field in colour_columns_by) { 
+            colour_by_out <- .choose_vis_values(object, field, mode = "column", search = "any",
+                                                exprs_values = exprs_values)
+
+            if (is.numeric(colour_by_out$val)) { 
+                colour_fac <- cut(colour_by_out$val, 25)
+            } else {
+                colour_fac <- as.factor(colour_by_out$val)
+            } 
+
+            nlevs_colour_by <- nlevels(colour_fac)
+            if (nlevs_colour_by <= 10) {
+                col_scale <- .get_palette("tableau10medium")
+            } else if (nlevs_colour_by > 10 && nlevs_colour_by <= 20) {
+                col_scale <- .get_palette("tableau20") 
+            } else {
+                col_scale <- viridis::viridis(nlevs_colour_by)                    
+            }
+
+            col_scale <- col_scale[seq_len(nlevs_colour_by)]
+            names(col_scale) <- levels(colour_fac)
+
+            column_variables[[colour_by_out$name]] <- colour_fac
+            column_colorings[[colour_by_out$name]] <- col_scale
+        }
+        column_variables <- do.call(data.frame, c(column_variables, list(row.names=colnames(object))))
+    } else {
+        column_variables <- column_colorings <- NULL
+    }
+
+    # Creating the heatmap as specified.
+    pheatmap::pheatmap(heat.vals, color=color, breaks=color.breaks, 
+        annotation_col=column_variables, annotation_colors=column_colorings, ...) 
+}
