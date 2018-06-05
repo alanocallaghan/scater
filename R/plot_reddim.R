088277e3c345d330506a0c5eb6bfa27084bc3d19
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-05 13:36:54 +0100

    Removed deprecated options for reduced dim plotting.

--- R/plot_reddim.R
+++ R/plot_reddim.R
@@ -8,12 +8,7 @@
 #' This can also be a numeric vector, see \code{?\link{plotReducedDim}} for details.
 #' @param ... Additional arguments to pass to \code{\link{plotReducedDim}}. 
 #' @param rerun Logical, should the reduced dimensions be recomputed even if \code{object} contains an appropriately named set of results in the \code{reducedDims} slot?
-#' @param return_SCE Logical, should the function return a SingleCellExperiment object with reduced dimension results in the \code{reducedDim} slot?
-#' Default is \code{FALSE}, in which case a \code{ggplot} object is returned. 
-#' This will be deprecated in the next release in favour of directly calling the underlying \code{run*} functions to compute the results.
-#' @param draw_plot Logical, should the plot be drawn on the current graphics device? 
-#' Only used if \code{return_SCE} is \code{TRUE}, otherwise the plot is always produced.
-#' @param run_args Arguments to pass to \code{\link{runPCA}}.
+#' @param run_args Arguments to pass to \code{\link{runPCA}}, \code{\link{runTSNE}}, etc.
 #'
 #' @details 
 #' Each function will search the \code{\link{reducedDims}} slot for an appropriately named set of results and pass those coordinates onto \code{\link{plotReducedDim}}.
@@ -30,7 +25,7 @@
 #' If \code{ncomponents} is a numeric vector, the maximum value will be used to determine the required number of dimensions to compute in the \code{run*} functions.
 #' However, only the specified dimensions in \code{ncomponents} will be plotted.
 #'
-#' @return A ggplot object or an SingleCellExperiment object, depending on \code{return_SCE}.
+#' @return A ggplot object.
 #'
 #' @author Davis McCarthy, with modifications by Aaron Lun
 #'
@@ -80,37 +75,33 @@
 #' ## Same for MDS plots:
 #' plotMDS(example_sce)
 #'
-plotPCASCE <- function(object, ..., return_SCE = FALSE, draw_plot = TRUE, rerun = FALSE, ncomponents = 2, run_args=list()) {
+plotPCASCE <- function(object, ..., rerun = FALSE, ncomponents = 2, run_args=list()) {
    .reddim_dispatcher(object=object, ncomponents=ncomponents, reddim_name="PCA", 
-                      rerun=rerun, reddim_FUN=runPCA, ..., run_args=run_args,
-                      return_SCE=return_SCE, draw_plot=draw_plot)
+                      rerun=rerun, reddim_FUN=runPCA, ..., run_args=run_args)
 }
 
 #' @rdname plot_reddim
 #' @aliases plotTSNE 
 #' @export
-plotTSNE <- function(object, ..., return_SCE = FALSE, draw_plot = TRUE, rerun = FALSE, ncomponents = 2, run_args=list()) {
+plotTSNE <- function(object, ..., rerun = FALSE, ncomponents = 2, run_args=list()) {
    .reddim_dispatcher(object=object, ncomponents=ncomponents, reddim_name="TSNE", 
-                      rerun=rerun, reddim_FUN=runTSNE, ..., run_args=run_args,
-                      return_SCE=return_SCE, draw_plot=draw_plot)
+                      rerun=rerun, reddim_FUN=runTSNE, ..., run_args=run_args)
 }
 
 #' @rdname plot_reddim
 #' @aliases plotDiffusionMap 
 #' @export
-plotDiffusionMap <- function(object, ..., return_SCE = FALSE, draw_plot = TRUE, rerun = FALSE, ncomponents = 2, run_args=list()) {
+plotDiffusionMap <- function(object, ..., rerun = FALSE, ncomponents = 2, run_args=list()) {
    .reddim_dispatcher(object=object, ncomponents=ncomponents, reddim_name="DiffusionMap",
-                      rerun=rerun, reddim_FUN=runDiffusionMap, ..., run_args=run_args,
-                      return_SCE=return_SCE, draw_plot=draw_plot)
+                      rerun=rerun, reddim_FUN=runDiffusionMap, ..., run_args=run_args)
 }
 
 #' @rdname plot_reddim
 #' @aliases plotMDS 
 #' @export
-plotMDS <- function(object, ..., ncomponents = 2, return_SCE = FALSE, rerun = FALSE, draw_plot = TRUE, run_args=list()) {
+plotMDS <- function(object, ..., rerun = FALSE, ncomponents=2, run_args=list()) {
    .reddim_dispatcher(object=object, ncomponents=ncomponents, reddim_name="MDS",
-                      rerun=rerun, reddim_FUN=runMDS, ..., run_args=run_args,
-                      return_SCE=return_SCE, draw_plot=draw_plot)
+                      rerun=rerun, reddim_FUN=runMDS, ..., run_args=run_args)
 }
 
 #' @rdname plot_reddim
@@ -118,47 +109,15 @@ plotMDS <- function(object, ..., ncomponents = 2, return_SCE = FALSE, rerun = FA
 #' @export
 setMethod("plotPCA", "SingleCellExperiment", plotPCASCE)
 
-
-.reddim_dispatcher <- function(object, ncomponents, reddim_name, rerun, reddim_FUN, ..., run_args, return_SCE, draw_plot) 
-# Central function to dispatch to the various run* function
-# and to plotReducedDim.
+#' @importFrom SingleCellExperiment reducedDimNames
+.reddim_dispatcher <- function(object, ncomponents, reddim_name, rerun, reddim_FUN, ..., run_args)
+# Central function to dispatch to the various run* functions and to plotReducedDim.
 {
-    new_args <- .disambiguate_args(...)    
-    run_args <- c(run_args, new_args$run)
-    plot_args <- new_args$plot
-
-    # Running the reduced dimension function if necessary.
-    if (!(reddim_name %in% names(reducedDims(object))) || rerun) {
+    if (!(reddim_name %in% reducedDimNames(object)) || rerun) {
         object <- do.call(reddim_FUN, c(list(object = object, ncomponents = max(ncomponents)), run_args))
     }
 
     ## Create the plot object
-    plot_out <- do.call(plotReducedDim, c(list(object = object, ncomponents = ncomponents, use_dimred = reddim_name), plot_args))
-
-    if (return_SCE) {
-        .Deprecated(msg="'return_SCE=TRUE' is deprecated, use 'runPCA' instead")
-        if ( draw_plot )
-            print(plot_out)
-        return(object)
-    } else {
-        return(plot_out)
-    }
+    plot_out <- plotReducedDim(object = object, ncomponents = ncomponents, use_dimred = reddim_name, ...)
+    return(plot_out)
 }
-
-.disambiguate_args <- function(...) 
-# This function is only necessary to provide some protection in the transition 
-# from having running arguments in "..." to plotting arguments in "...". It can
-# be removed in the next development cycle. 
-{
-    plot_arg_names <- Reduce(union, list(names(formals(plotReducedDim)), names(formals(.central_plotter)), names(formals(paired_reddim_plot))))
-    extra_args <- list(...)
-    for_plotting <- !is.na(pmatch(names(extra_args), plot_arg_names))
-    if (!all(for_plotting)) { 
-        warning(sprintf("non-plotting arguments like '%s' should go in 'run_args'", 
-                        names(extra_args)[!for_plotting][1]))
-    }
-    return(list(plot=extra_args[for_plotting],
-                run=extra_args[!for_plotting]))
-}
-
-
