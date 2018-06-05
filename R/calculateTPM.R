01a0012e705d92f8a54e75ffa713ecf3a0d2408e
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-05 14:05:48 +0100

    Streamlined calculateTPM, improved documentation.

---
+++ R/calculateTPM.R
@@ -0,0 +1,74 @@
+#' Calculate which features are expressed in which cells using a threshold on
+#' observed counts, transcripts-per-million, counts-per-million, FPKM, or
+#' defined expression levels.
+#'
+#' @param object a \code{\link{SingleCellExperiment}} object with expression 
+#' and/or count data.
+#' @param detection_limit numeric scalar giving the minimum expression level
+#' for an expression observation in a cell for it to qualify as expressed.
+#' @param exprs_values character scalar indicating whether the count data
+#' (\code{"counts"}), the log-transformed count data (\code{"logcounts"}),
+#' transcript-per-million (\code{"tpm"}), counts-per-million (\code{"cpm"}) or
+#' FPKM (\code{"fpkm"}) should be used to define if an observation is expressed
+#' or not. Defaults to the first available value of those options in the
+#' order shown.
+#' @return a logical matrix indicating whether or not a feature in a particular
+#' cell is expressed.
+#' 
+#' @export
+#' @examples
+#' data("sc_example_counts")
+#' data("sc_example_cell_info")
+#' example_sce <- SingleCellExperiment(
+#' assays = list(counts = sc_example_counts), colData = sc_example_cell_info)
+#' assay(example_sce, "is_exprs") <- calcIsExprs(example_sce, 
+#' detection_limit = 1, exprs_values = "counts")
+calcIsExprs <- function(object, detection_limit = 0, 
+                        exprs_values = "counts") {
+    .Deprecated(msg="'calcIsExprs' is deprecated.\nUse 'counts(object) > 0' instead")
+    assay(object, i = exprs_values) > detection_limit
+}
+
+#' Calculate transcripts-per-million (TPM)
+#'
+#' Calculate transcripts-per-million (TPM) values for expression from counts for a set of features.
+#'
+#' @param object A SingleCellExperiment object or a count matrix.
+#' @param exprs_values String specifying the assay containing the counts in \code{object}, if it is a SingleCellExperiment.
+#' @param effective_length Numeric vector containing the effective length for each feature in \code{object}.
+#' If \code{NULL}, it is assumed that \code{exprs_values} has already been adjusted for transcript length.
+#'
+#' @details
+#' For read count data, this function assumes uniform coverage along the (effective) length of the transcript.
+#' Thus, the number of transcripts for a gene is proportional to the read count divided by the transcript length.
+#'
+#' For UMI count data, this function should be run with \code{effective_length=NULL}, i.e., no division by the effective length.
+#' This is because the number of UMIs is a direct (albeit probably biased) estimate of the number of transcripts.
+#'
+#' @return A numeric matrix of TPM values.
+#' @export
+#' @examples
+#' data("sc_example_counts")
+#' data("sc_example_cell_info")
+#' example_sce <- SingleCellExperiment(
+#'     assays = list(counts = sc_example_counts), 
+#'     colData = sc_example_cell_info)
+#'
+#' eff_len <- runif(nrow(example_sce), 500, 2000)
+#' tout <- calculateTPM(example_sce, effective_length = eff_len)
+#'
+#'
+#' @importClassesFrom SingleCellExperiment SingleCellExperiment
+#' @importFrom methods is
+#' @importFrom SummarizedExperiment assay
+calculateTPM <- function(object, exprs_values= "counts", effective_length=NULL) {
+    if (is(object, "SingleCellExperiment") ) {
+        object <- assay(object, i=exprs_values)
+    }
+
+    if (!is.null(effective_length)) {
+        object <- object/effective_length
+    }
+
+    calculateCPM(object)
+}
