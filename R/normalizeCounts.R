8636f6663c76913f6480846b2d855e762cef7f24
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-02 13:18:52 +0100

    Renamed normalizeMatrix to normalizeCounts, added subset_row arg.

--- R/normalizeCounts.R
+++ R/normalizeCounts.R
@@ -7,6 +7,7 @@
 #' @param return_log Logical scalar, should normalized values be returned on the log2 scale? 
 #' @param log_exprs_offset Numeric scalar specifying the offset to add when log-transforming expression values.
 #' @param centre_size_factors Logical scalar indicating whether size fators should be centred.
+#' @param subset_row A vector specifying the subset of rows of \code{x} for which to return a result.
 #'
 #' @details 
 #' This function is more memory-efficient than \code{t(t(x)/size_factors)},
@@ -22,16 +23,18 @@
 #' @export
 #' @examples
 #' data("sc_example_counts")
-#' normed <- normalizeMatrix(sc_example_counts, 
+#' normed <- normalizeCounts(sc_example_counts, 
 #'     librarySizeFactors(sc_example_counts))
-normalizeMatrix <- function(x, size_factors, return_log = TRUE, log_exprs_offset = 1, centre_size_factors = FALSE) {
+normalizeCounts <- function(x, size_factors, return_log = TRUE, log_exprs_offset = 1, centre_size_factors = FALSE, subset_row = NULL) {
     if (centre_size_factors) {
         size_factors <- size_factors/mean(size_factors)
     }
-
+    
+    subset_row <- .subset2index(subset_row, x, byrow=TRUE)
     out <- .Call(cxx_norm_exprs, x, list(size_factors), integer(nrow(x)),
-        log_exprs_offset, return_log, seq_len(nrow(x)) - 1L)
+        log_exprs_offset, return_log, subset_row - 1L)
 
-    dimnames(out) <- dimnames(x)
+    colnames(out) <- colnames(x)
+    rownames(out) <- rownames(x)[subset_row]
     return(out)
 }
