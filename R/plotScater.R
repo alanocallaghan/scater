7fb10c4646baec747275e58f7017297ce78b491f
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-02 17:12:33 +0100

    Updated plotScater for new C++ code.

--- R/plotScater.R
+++ R/plotScater.R
@@ -83,10 +83,10 @@ plotScater <- function(x, nfeatures = 500, exprs_values = "counts",
     ## Use C++ to get the sequencing real estate accounted for by features
     to_plot <- seq_len(nfeatures)
     ncells <- ncol(exprs_mat)
-    seq_real_estate <- .Call(cxx_calc_top_features, exprs_mat, to_plot, NULL)
+    seq_real_estate <- .Call(cxx_top_cumprop, exprs_mat, to_plot)
     seq_real_estate_long <- data.frame(Feature=rep(to_plot, each=ncells),
                                        Cell=rep(seq_len(ncells), nfeatures))
-    seq_real_estate_long$Proportion_Library <- unlist(seq_real_estate) / 100
+    seq_real_estate_long$Proportion_Library <- as.vector(t(seq_real_estate))
 
     ## Add block and colour_by information if provided
     seq_real_estate_long$block1 <- rep(block1_vals, nfeatures)
