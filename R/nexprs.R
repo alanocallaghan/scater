518e4226e136882bf45a50e5dbd6c8f680e2771b
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-22 11:32:04 +0100

    Support parallelization in nexprs().

--- R/nexprs.R
+++ R/nexprs.R
@@ -10,6 +10,7 @@
 #' If \code{FALSE}, the function will count the number of detected features per cell.
 #' @param subset_row Logical, integer or character vector indicating which rows (i.e. features) to use.
 #' @param subset_col Logical, integer or character vector indicating which columns (i.e., cells) to use.
+#' @param BPPARAM A BiocParallelParam object specifying whether the calculations should be parallelized. 
 #'
 #' @details 
 #' Setting \code{subset_row} or \code{subset_col} is equivalent to subsetting \code{object} before calling \code{nexprs}, 
@@ -31,8 +32,9 @@
 #' @importClassesFrom SingleCellExperiment SingleCellExperiment
 #' @importFrom SummarizedExperiment assay
 #' @importFrom methods is
+#' @importFrom BiocParallel bplapply SerialParam
 nexprs <- function(object, detection_limit = 0, exprs_values = "counts", 
-    byrow = FALSE, subset_row = NULL, subset_col = NULL) 
+    byrow = FALSE, subset_row = NULL, subset_col = NULL, BPPARAM=SerialParam()) 
 {
     subset_row <- .subset2index(subset_row, target = object, byrow = TRUE)
     subset_col <- .subset2index(subset_col, target = object, byrow = FALSE)
@@ -43,9 +45,41 @@ nexprs <- function(object, detection_limit = 0, exprs_values = "counts",
         exprs_mat <- object
     }
 
+    # Zero-indexing.
+    subset_by_worker <- .split_vector_by_workers(subset_col - 1L, BPPARAM)
+    zero_subset_row <- subset_row - 1L
+
     if (!byrow) {
-        return(.Call(cxx_col_above, exprs_mat, subset_row - 1L, subset_col - 1L, detection_limit))
+        bp_out <- bplapply(subset_by_worker, FUN=.get_detected_per_col,
+            mat=exprs_mat,
+            subset_row=zero_subset_row, 
+            detection_limit=detection_limit,
+            BPPARAM=BPPARAM)
+        
+        ndetected <- unlist(bp_out)
+        names(ndetected) <- colnames(object)[subset_col]
+        return(ndetected)
+
     } else {
-        return(.Call(cxx_row_above, exprs_mat, subset_row - 1L, subset_col - 1L, detection_limit))
+        bp_out <- bplapply(subset_by_worker, FUN=.get_detected_per_row,
+            mat=exprs_mat,
+            subset_row=zero_subset_row, 
+            detection_limit=detection_limit,
+            BPPARAM=BPPARAM)
+        
+        ndetected <- Reduce("+", bp_out)
+        names(ndetected) <- rownames(object)[subset_row]
+        return(ndetected)
     }
+}
+
+.get_detected_per_col <- function(mat, subset_row, subset_col, detection_limit) 
+# A helper function defined in the scater namespace.
+# This avoids the need to reattach scater in bplapply for SnowParam().
+{
+    .Call(cxx_col_above, mat, subset_row, subset_col, detection_limit)
+}
+
+.get_detected_per_row <- function(mat, subset_row, subset_col, detection_limit) {
+    .Call(cxx_row_above, mat, subset_row, subset_col, detection_limit)
 }
