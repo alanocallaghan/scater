2810f7e8414668c2f46c2a560adf7324ce70007e
Aaron Lun <alun@wehi.edu.au>
2018-12-08 18:16:48 +0000

    Support external generation and use of NN results in runTSNE.

--- R/runTSNE.R
+++ R/runTSNE.R
@@ -17,6 +17,11 @@
 #' @param pca Logical scalar passed to \code{\link[Rtsne]{Rtsne}}, indicating whether an initial PCA step should be performed.
 #' This is ignored if \code{use_dimred} is specified.
 #' @param initial_dims Integer scalar passed to \code{\link[Rtsne]{Rtsne}}, specifying the number of principal components to be retained if \code{pca=TRUE}. 
+#' @param normalize Logical scalar indicating if input values should be scaled for numerical precision, see \code{\link[Rtsne]{normalize_input}}.
+#' @param theta Numeric scalar specifying the approximation accuracy of the Barnes-Hut algorithm, see \code{\link[Rtsne]{Rtsne}} for details.
+#' @param external_neighbors Logical scalar indicating whether a nearest neighbors search should be computed externally with \code{\link{findKNN}}.
+#' @param BNPARAM A \linkS4class{BiocNeighborParam} object specifying the neighbor search algorithm to use when \code{external_neighbors=TRUE}.
+#' @param BPPARAM A \linkS4class{BiocParallelParam} object specifying how the neighbor search should be parallelized when \code{external_neighbors=TRUE}.
 #' @param ... Additional arguments to pass to \code{\link[Rtsne]{Rtsne}}.
 #'
 #' @return 
@@ -35,6 +40,10 @@
 #' Setting \code{use_dimred} allows users to easily perform t-SNE on low-rank approximations of the original expression matrix (e.g., after PCA).
 #' In such cases, arguments such as \code{ntop}, \code{feature_set}, \code{exprs_values} and \code{scale_features} will be ignored. 
 #'
+#' If \code{external_neighbors=TRUE}, the nearest neighbor search step is conducted using a different algorithm to that in the \code{\link[Rtsne]{Rtsne}} function.
+#' This can be parallelized or approximate to achieve greater speed for large data sets.
+#' The neighbor search results are then used for t-SNE via the \code{\link[Rtsne]{Rtsne_neighbors}} function.
+#'
 #' @references
 #' L.J.P. van der Maaten. Barnes-Hut-SNE. In Proceedings of the International Conference on Learning Representations, 2013.
 #'
@@ -43,6 +52,8 @@
 #' \code{\link[Rtsne]{Rtsne}},
 #' \code{\link[scater]{plotTSNE}}
 #' @export
+#' @importFrom BiocNeighbors findKNN
+#' @importFrom BiocParallel SerialParam
 #'
 #' @author Aaron Lun, based on code by Davis McCarthy
 #'
@@ -63,7 +74,10 @@ runTSNE <- function(object, ncomponents = 2, ntop = 500, feature_set = NULL,
         exprs_values = "logcounts", scale_features = TRUE,
         use_dimred = NULL, n_dimred = NULL, 
         perplexity = min(50, floor(ncol(object) / 5)), 
-        pca = TRUE, initial_dims = 50, ...) 
+        pca = TRUE, initial_dims = 50, 
+        normalize = TRUE, theta = 0.5,
+        external_neighbors = FALSE, BNPARAM = NULL, BPPARAM = SerialParam(),
+        ...) 
 {
     if (!is.null(use_dimred)) {
         ## Use existing dimensionality reduction results (turning off PCA)
@@ -79,9 +93,20 @@ runTSNE <- function(object, ncomponents = 2, ntop = 500, feature_set = NULL,
         initial_dims <- min(initial_dims, ncol(vals))
     }
 
-    # Actually running the Rtsne step.
     vals <- as.matrix(vals) # protect against alternative matrix inputs.
-    tsne_out <- Rtsne::Rtsne(vals, initial_dims = initial_dims, pca = pca, perplexity = perplexity, dims = ncomponents, check_duplicates = FALSE, ...)
+
+    # Actually running the Rtsne step.
+    if (!external_neighbors || theta==0) {
+        tsne_out <- Rtsne::Rtsne(vals, initial_dims = initial_dims, pca = pca, perplexity = perplexity, dims = ncomponents, check_duplicates = FALSE,
+            normalize=normalize, theta=theta, ...)
+    } else {
+        if (normalize) {
+            vals <- Rtsne::normalize_input(vals)
+        }
+        nn_out <- findKNN(vals, k=floor(3*perplexity), BNPARAM=BNPARAM, BPPARAM=BPPARAM)
+        tsne_out <- Rtsne::Rtsne_neighbors(nn_out$index, nn_out$distance, perplexity=perplexity, dims=ncomponents, theta=theta, ...)
+    }
+
     reducedDim(object, "TSNE") <- tsne_out$Y
     return(object)
 }
