52f8eef7ce5e5a7c91784303a495bc30c9447400
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-01-18 19:06:06 +0000

    More flexible getExplanatoryPCs for existing/new PC inputs.

--- R/getExplanatoryPCs.R
+++ R/getExplanatoryPCs.R
@@ -1,9 +1,10 @@
 #' Estimate the percentage of variance explained for each PC. 
 #'
 #' @param object A SingleCellExperiment object containing expression values and per-cell experimental information.
-#' @param ncomponents Integer scalar specifying the number of the top principal components to use.
-#' Ignored if \code{use_dimred} is specified.
 #' @param use_dimred String specifying the field in \code{reducedDims(object)} that contains the PCA results.
+#' @param ncomponents Integer scalar specifying the number of the top principal components to use.
+#' @param rerun Logical scalar indicating whether the PCA should be repeated, even if pre-computed results are already present.
+#' @param run_args A named list of arguments to pass to \code{\link[scater]{runPCA}}.
 #' @param ... Additional arguments passed to \code{\link{getVarianceExplained}}.
 #'
 #' @details 
@@ -11,8 +12,9 @@
 #' It allows identification of important PCs that are driven by known experimental conditions, e.g., treatment, disease.
 #' PCs correlated with technical factors (e.g., batch effects, library size) can also be detected and removed prior to further analysis.
 #'
-#' If \code{use_dimred} is not \code{NULL}, the function will attempt to extract existing PCA results in \code{object}.
-#' Otherwise, it will rerun the PCA using \code{\link{runPCA}} for the top \code{ncomponents} components.
+#' By default, the function will attempt to use pre-computed PCA results in \code{object}.
+#' This is done by taking the top \code{ncomponents} PCs from the matrix identified by \code{use_dimred}.
+#' If these are not available or if \code{rerun=TRUE}, the function will rerun the PCA using \code{\link[scater]{runPCA}}.
 #'
 #' @return
 #' A matrix containing the percentage of variance explained by each factor (column) and for each PC (row).
@@ -24,7 +26,8 @@
 #' @author Aaron Lun
 #'
 #' @export
-#' @importFrom SingleCellExperiment reducedDim SingleCellExperiment
+#' @importFrom SingleCellExperiment reducedDim reducedDimNames SingleCellExperiment
+#' @importFrom SummarizedExperiment colData
 #' 
 #' @examples
 #' data("sc_example_counts")
@@ -35,11 +38,14 @@
 #' example_sce <- normalize(example_sce)
 #'                                                    
 #' r2mat <- getExplanatoryPCs(example_sce)
-getExplanatoryPCs <- function(object, ncomponents=10, use_dimred = NULL, ...) {
-    if (is.null(reddims)) {
-        reddims <- reducedDim(runPCA(object, ncomponents=ncomponents), "PCA")
+getExplanatoryPCs <- function(object, use_dimred = "PCA", ncomponents=10, rerun=FALSE, run_args=list(), ...) {
+    if (!use_dimred %in% reducedDimNames(object) || rerun) {
+        object <- do.call(runPCA, c(list(object=object, ncomponents=ncomponents), run_args))
+        reddims <- reducedDim(object, "PCA")
     } else {
         reddims <- reducedDim(object, use_dimred)
+        ncomponents <- min(ncomponents, ncol(reddims))
+        reddims <- reddims[,seq_len(ncomponents),drop=FALSE]        
     }
 
     dummy <- SingleCellExperiment(list(pc_space=t(reddims)), colData=colData(object))
