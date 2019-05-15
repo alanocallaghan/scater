2c4865fd2e6be7c2135904bfd0bcfde7a7e2e056
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-05-14 17:48:18 -0700

    Deprecated dplyr verbs.

--- R/verbs.R
+++ R/verbs.R
@@ -2,7 +2,7 @@
 
 # Mutate verb -------------------------------------------------------------
 
-#' Add new variables to \code{colData(object)}.
+#' Add new variables to \code{colData(object)} (deprecated).
 #'
 #' @param object a \code{SingleCellExperiment} object.
 #' @param ... Additional arguments to be passed to \code{dplyr::mutate} to
@@ -15,16 +15,13 @@
 #' @name mutate
 #' @aliases mutate mutate,SingleCellExperiment-method
 #'
-#' @examples
-#' data("sc_example_counts")
-#' data("sc_example_cell_info")
-#' example_sce <- SingleCellExperiment(
-#' assays = list(counts = sc_example_counts), 
-#' colData = sc_example_cell_info)
-#' example_sce <- mutate(example_sce, is_quiescent = Cell_Cycle == "G0")
+#' @details 
+#' Refer to \url{https://github.com/sa-lee/plyexperiment} for replacement functionality.
+#'
 #' @rdname mutate
 #' @export
 setMethod("mutate", "SingleCellExperiment", function(object, ...) {
+    .Deprecated()
     pd <- as.data.frame(colData(object))
     pd <- dplyr::mutate(pd, ...)
     rownames(pd) <- colnames(object)
@@ -35,7 +32,7 @@ setMethod("mutate", "SingleCellExperiment", function(object, ...) {
 
 # Rename verb -------------------------------------------------------------
 
-#' Rename variables of \code{colData(object)}.
+#' Rename variables of \code{colData(object)} (deprecated).
 #'
 #' @param object A \code{SingleCellExperiment} object.
 #' @param ... Additional arguments to be passed to \code{dplyr::rename} to
@@ -48,15 +45,11 @@ setMethod("mutate", "SingleCellExperiment", function(object, ...) {
 #' @name rename
 #' @aliases rename rename,SingleCellExperiment-method
 #'
-#' @examples
-#' data("sc_example_counts")
-#' data("sc_example_cell_info")
-#' example_sce <- SingleCellExperiment(
-#' assays = list(counts = sc_example_counts), 
-#' colData = sc_example_cell_info)
-#' example_sce <- rename(example_sce, Cell_Phase = Cell_Cycle)
+#' @details 
+#' Refer to \url{https://github.com/sa-lee/plyexperiment} for replacement functionality.
 #' 
 setMethod("rename", "SingleCellExperiment", function(object, ...) {
+    .Deprecated()
     pd <- as.data.frame(colData(object))
     pd <- dplyr::rename(pd, ...)
     rownames(pd) <- colnames(object)
@@ -66,7 +59,7 @@ setMethod("rename", "SingleCellExperiment", function(object, ...) {
 
 # Filter verb -------------------------------------------------------------
 
-#' Return \code{SingleCellExperiment} with cells matching conditions.
+#' Return \code{SingleCellExperiment} with cells matching conditions (deprecated).
 #'
 #' Subsets the columns (cells) of a \code{SingleCellExperiment} based on
 #' matching conditions in the rows of \code{colData(object)}.
@@ -77,20 +70,15 @@ setMethod("rename", "SingleCellExperiment", function(object, ...) {
 #'
 #' @return An SingleCellExperiment object.
 #'
+#' @details 
+#' Refer to \url{https://github.com/sa-lee/plyexperiment} for replacement functionality.
+#'
 #' @export
 #' @rdname filter
 #' @name filter
 #' @aliases filter filter,SingleCellExperiment-method
-#'
-#' @examples
-#' data("sc_example_counts")
-#' data("sc_example_cell_info")
-#' example_sce <- SingleCellExperiment(
-#' assays = list(counts = sc_example_counts), 
-#' colData = sc_example_cell_info)
-#' example_sce_treat1 <- filter(example_sce, Treatment == "treat1")
-#' 
 setMethod("filter", "SingleCellExperiment", function(object, ...) {
+    .Deprecated()
     pd <- as.data.frame(colData(object))
     pd <- dplyr::mutate(pd, scater_placeholder_index = seq_len(nrow(pd)))
     pd <- dplyr::filter(pd, ...) # filter not exported by dplyr
@@ -99,7 +87,7 @@ setMethod("filter", "SingleCellExperiment", function(object, ...) {
 
 # Arrange verb ------------------------------------------------------------
 
-#' Arrange columns (cells) of a SingleCellExperiment object
+#' Arrange columns (cells) of a SingleCellExperiment object (deprecated).
 #'
 #' The \code{SingleCellExperiment} returned will have cells ordered by the corresponding
 #' variable in \code{colData(object)}.
@@ -110,20 +98,15 @@ setMethod("filter", "SingleCellExperiment", function(object, ...) {
 #'
 #' @return An SingleCellExperiment object.
 #'
+#' @details 
+#' Refer to \url{https://github.com/sa-lee/plyexperiment} for replacement functionality.
+#'
 #' @export
 #' @rdname arrange
 #' @name arrange
 #' @aliases arrange arrange,SingleCellExperiment-method
-#'
-#' @examples
-#' data("sc_example_counts")
-#' data("sc_example_cell_info")
-#' example_sce <- SingleCellExperiment(
-#' assays = list(counts = sc_example_counts), 
-#' colData = sc_example_cell_info)
-#' example_sce <- arrange(example_sce, Cell_Cycle)
-#' 
 setMethod("arrange", "SingleCellExperiment", function(object, ...) {
+    .Deprecated()
     pd <- as.data.frame(colData(object))
     pd <- dplyr::mutate(pd, scater_placeholder_index = seq_len(nrow(pd)))
     pd <- dplyr::arrange(pd, ...) # arrange not exported by dplyr
