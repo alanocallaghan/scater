09ee5fb43b5e3dee7ef9e91510d94a032c3f8867
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-20 11:07:28 -0700

    Converted matrix calculators to S4 where necessary.

--- R/calculateTPM.R
+++ R/calculateTPM.R
@@ -1,22 +1,39 @@
-#' Calculate transcripts-per-million (TPM)
+#' Calculate TPMs
 #'
-#' Calculate transcripts-per-million (TPM) values for expression from counts for a set of features.
+#' Calculate transcripts-per-million (TPM) values for expression from feature-level counts.
 #'
-#' @param object A SingleCellExperiment object or a count matrix.
-#' @param effective_length Numeric vector containing the effective length for each feature in \code{object}.
-#' If \code{NULL}, it is assumed that \code{exprs_values} has already been adjusted for transcript length.
-#' @param exprs_values String or integer specifying the assay containing the counts in \code{object}, if it is a SingleCellExperiment.
-#' @param subset_row A vector specifying the subset of rows of \code{object} for which to return a result.
+#' @param x A numeric matrix of counts where features are rows and cells are columns.
+#'
+#' Alternatively, a \linkS4class{SummarizedExperiment} or a \linkS4class{SingleCellExperiment} containing such counts.
+#' @param effective.length Numeric vector providing the effective length for each feature in \code{x}.
+#' Alternatively \code{NULL}, see Details.
+#' @param effective_length Deprecated, same as \code{effective.length}.
+#' @param assay.type A string specifying the assay of \code{x} containing the count matrix.
+#' @param exprs_values Deprecated, same as \code{assay.type}.
+#' @param ... For the generic, arguments to pass to specific methods.
+#'
+#' For the ANY method, further arguments to pass to \code{\link{calculateCPM}}.
+#'
+#' For the SummarizedExperiment method, further arguments to pass to the ANY method.
+#'
+#' For the SingleCellExperiment method, further arguments to pass to the SummarizedExperiment method.
+#' @param alt.exp String or integer scalar specifying an alternative experiment for which to compute the averages.
 #'
 #' @details
 #' For read count data, this function assumes uniform coverage along the (effective) length of the transcript.
 #' Thus, the number of transcripts for a gene is proportional to the read count divided by the transcript length.
+#' Here, the division is done before calculation of the library size to compute per-million values,
+#' where \code{\link{calculateFPKM}} will only divide by the length after library size normalization.
 #'
 #' For UMI count data, this function should be run with \code{effective_length=NULL}, i.e., no division by the effective length.
-#' This is because the number of UMIs is a direct (albeit probably biased) estimate of the number of transcripts.
+#' This is because the number of UMIs is a direct (albeit biased) estimate of the number of transcripts.
 #'
+#' @name calculateTPM
 #' @return A numeric matrix of TPM values.
-#' @export
+#' @author Aaron Lun, based on code by Davis McCarthy
+#' @seealso
+#' \code{\link{calculateCPM}}, on which this function is based.
+#'
 #' @examples
 #' data("sc_example_counts")
 #' data("sc_example_cell_info")
@@ -25,20 +42,39 @@
 #'     colData = sc_example_cell_info)
 #'
 #' eff_len <- runif(nrow(example_sce), 500, 2000)
-#' tout <- calculateTPM(example_sce, effective_length = eff_len)
-#'
-#'
-#' @importClassesFrom SingleCellExperiment SingleCellExperiment
-#' @importFrom methods is
-#' @importFrom SummarizedExperiment assay
-calculateTPM <- function(object, effective_length=NULL, exprs_values = "counts", subset_row = NULL) {
-    if (is(object, "SingleCellExperiment") ) {
-        object <- assay(object, i=exprs_values)
-    }
+#' tout <- calculateTPM(example_sce, effective.length = eff_len)
+#' str(tout)
+NULL
 
-    if (!is.null(effective_length)) {
-        object <- object/effective_length
+.calculate_tpm <- function(x, effective.length, ...) {
+    if (!is.null(effective.length)) {
+        x <- x/effective.length
     }
-
-    calculateCPM(object, subset_row = subset_row)
+    .calculate_cpm(x, ...)
 }
+
+#' @export
+#' @rdname calculateTPM
+setMethod("calculateTPM", "ANY", .calculate_tpm)
+
+#' @export
+#' @rdname calculateTPM
+#' @importFrom SummarizedExperiment assay assay<-
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
+setMethod("calculateTPM", "SummarizedExperiment", function(x, ..., assay.type="counts", exprs_values=NULL) {
+    assay.type <- .switch_arg_names(exprs_values, assay.type)
+    .calculate_tpm(assay(x, assay.type), ...)
+})
+
+#' @export
+#' @rdname calculateTPM
+#' @importFrom SingleCellExperiment altExp
+#' @importClassesFrom SingleCellExperiment SingleCellExperiment
+setMethod("calculateTPM", "SingleCellExperiment", function(x, ..., alt.exp=NULL) {
+    if (!is.null(alt.exp)) {
+        y <- altExp(x, alt.exp)
+        calculateTPM(y, ...)
+    } else {
+        callNextMethod(x=x, ...)
+    }
+})
