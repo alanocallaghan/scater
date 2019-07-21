a0d282503347481a97d2d43ce50c875a3a1ab2a5
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-21 16:49:36 -0700

    Overhauled runColDataPCA.

--- R/runColDataPCA.R
+++ R/runColDataPCA.R
@@ -6,28 +6,24 @@
 #' @param x A \linkS4class{SingleCellExperiment} object.
 #' @param ncomponents Numeric scalar indicating the number of principal components to obtain.
 #' This will override any \code{ntop} argument if specified.
-#' @param scale_features Logical scalar, should the expression values be standardised so that each feature has unit variance?
-#' This will also remove features with standard deviations below 1e-8. 
-#' @param selected_variables List of strings or a character vector indicating which variables in \code{colData(x)} to use.
+#' @param variables List of strings or a character vector indicating which variables in \code{colData(x)} to use.
 #' If a list, each entry can also be an \link{AsIs} vector or a data.frame, as described in \code{?\link{retrieveCellInfo}}.
-#' @param detect_outliers Logical indicating whether outliers should be detected based on PCA coordinates.
+#' @param selected_variables Deprecated, same as \code{variables}.
+#' @param scale Logical scalar, should the expression values be standardised so that each feature has unit variance?
+#' This will also remove features with standard deviations below 1e-8. 
+#' @param scale_features Deprecated, same as \code{scale}.
+#' @param outliers Logical indicating whether outliers should be detected based on PCA coordinates.
+#' @param detect_outliers Deprecated, same as \code{outliers}.
 #' @param BSPARAM A \linkS4class{BiocSingularParam} object specifying which algorithm should be used to perform the PCA.
 #' @param BPPARAM A \linkS4class{BiocParallelParam} object specifying whether the PCA should be parallelized.
 #' @param name String specifying the name to be used to store the result in the \code{reducedDims} of the output.
 #'
 #' @details 
-#' This function performs PCA on column-level metadata instead of the gene expression matrix. 
-#' The \code{selected_variables} defaults to a vector containing:
-#' \itemize{
-#' \item \code{"pct_counts_top_100_features"}
-#' \item \code{"total_features_by_counts"}
-#' \item \code{"pct_counts_feature_control"}
-#' \item \code{"total_features_feature_control"}
-#' \item \code{"log10_total_counts_endogenous"}
-#' \item \code{"log10_total_counts_feature_control"}
-#' }
-#'
-#' The default variables are useful for identifying outliers cells based on QC metrics when \code{detect_outliers=TRUE}.
+#' This function performs PCA on variables from the column-level metadata instead of the gene expression matrix. 
+#' Doing so can be occasionally useful when other forms of experimental data are stored in the \code{colData},
+#' e.g., protein intensities from FACs or other cell-specific phenotypic information.
+#' 
+#' This function is particularly useful for identifying low-quality cells based on QC metrics with \code{outliers=TRUE}.
 #' This uses an \dQuote{outlyingness} measure computed by \code{adjOutlyingness} in the \pkg{robustbase} package.
 #' Outliers are defined those cells with outlyingness values more than 5 MADs above the median, using \code{\link{isOutlier}}.
 #'
@@ -35,25 +31,29 @@
 #' By default, these are stored in the \code{"PCA_coldata"} entry of the \code{reducedDims} slot.
 #' The proportion of variance explained by each PC is stored as a numeric vector in the \code{"percentVar"} attribute.
 #'
-#' If \code{detect_outliers=TRUE}, the output \code{colData} will also contain a logical \code{outlier} field.
+#' If \code{outliers=TRUE}, the output \code{colData} will also contain a logical \code{outlier} field.
 #' This specifies the cells that correspond to the identified outliers.
 #' 
 #' @seealso \code{\link[scater]{runPCA}}, for the corresponding method operating on expression data.
 #'
 #' @author Aaron Lun, based on code by Davis McCarthy
 #' @examples
-#' ## Set up an example SingleCellExperiment
 #' data("sc_example_counts")
 #' data("sc_example_cell_info")
 #' example_sce <- SingleCellExperiment(
 #'     assays = list(counts = sc_example_counts),
 #'     colData = sc_example_cell_info
 #' )
-#' example_sce <- normalize(example_sce)
 #'
-#' example_sce <- calculateQCMetrics(example_sce,
-#'     feature_controls=list(Spike=1:10))
-#' example_sce <- runColDataPCA(example_sce)
+#' qc.df <- perCellQCMetrics(example_sce, subset=list(Mito=1:10))
+#' colData(example_sce) <- cbind(colData(example_sce), qc.df)
+#' 
+#' # Can supply names of colData variables to 'variables',
+#' # as well as AsIs-wrapped vectors of interest.
+#' example_sce <- runColDataPCA(example_sce, variables=list(
+#'     "sum", "above.limit", # variable names 
+#'      I(log1p(qc.df$subsets$Mito$sum)) # AsIs-wrapped vectors
+#' ))
 #' reducedDimNames(example_sce)
 #' head(reducedDim(example_sce))
 #'
@@ -62,11 +62,15 @@
 #' @importFrom DelayedArray DelayedArray 
 #' @importFrom BiocSingular runPCA ExactParam 
 #' @importFrom BiocParallel SerialParam
-runColDataPCA <- function(x, ncomponents = 2, scale_features = TRUE,
-    selected_variables = NULL, detect_outliers = FALSE,
+runColDataPCA <- function(x, ncomponents = 2, 
+    variables=NULL, selected_variables=NULL,
+    scale=TRUE, scale_features = NULL,
+    outliers = FALSE, detect_outliers = NULL,
     BSPARAM = ExactParam(), BPPARAM = SerialParam(), name = "PCA_coldata")
 {
-    if (is.null(selected_variables)) {
+    variables <- .switch_arg_names(selected_variables, variables)
+    if (is.null(variables)) {
+        .Deprecated(msg="'variables' must now be explicitly specified")
         candidates <- c("pct_counts_in_top_100_features",
             "total_features_by_counts",
             "pct_counts_feature_control",
@@ -75,35 +79,41 @@ runColDataPCA <- function(x, ncomponents = 2, scale_features = TRUE,
             "log10_total_counts_feature_control")
 
         # Fishing out the (possibly compacted) metadata fields.
-        selected_variables <- list()
+        variables <- list()
         it <- 1L
         for (field in candidates) {
             out <- .qc_hunter(x, field, mode = "column", error = FALSE)
             if (!is.null(out)) {
-                selected_variables[[it]] <- out
+                variables[[it]] <- out
                 it <- it + 1L
             }
         }
     }
 
     # Constructing a matrix - presumably all doubles.
-    exprs_to_plot <- matrix(0, ncol(x), length(selected_variables))
-    for (it in seq_along(selected_variables)) {
-        exprs_to_plot[,it] <- retrieveCellInfo(x, selected_variables[[it]], search = "colData")$val
-    }
-    if (scale_features) {
-        exprs_to_plot <- .scale_columns(exprs_to_plot)
+    exprs_to_plot <- matrix(0, ncol(x), length(variables))
+    for (it in seq_along(variables)) {
+        exprs_to_plot[,it] <- retrieveCellInfo(x, variables[[it]], search = "colData")$val
     }
 
-    ## Outlier detection. We used to use mvoutlier but their dependency tree 
-    ## changed to require system libraries, which were untenable.
-    if (detect_outliers) {
+    scale <- .switch_arg_names(scale_features, scale)
+    cv <- colVars(exprs_to_plot)
+    if (scale) {
+        keep <- cv >= 1e-8
+        exprs_to_plot <- sweep(exprs_to_plot[,keep,drop=FALSE], 2, sqrt(cv[keep]), "/")
+        cv <- rep(1, ncol(exprs_to_plot))
+    }
+
+    # Outlier detection. We used to use mvoutlier but their dependency tree 
+    # changed to require system libraries, which were untenable.
+    outliers <- .switch_arg_names(detect_outliers, outliers)
+    if (outliers) {
         outlying <- robustbase::adjOutlyingness(exprs_to_plot, only.outlyingness=TRUE)
         x$outlier <- isOutlier(outlying, type="higher")
     }
 
     pca <- runPCA(exprs_to_plot, rank=ncomponents, BSPARAM=BSPARAM, BPPARAM=BPPARAM, get.rotation=FALSE)
-    percentVar <- pca$sdev ^ 2 / sum(colVars(DelayedArray(exprs_to_plot))) # as not all singular values are computed.
+    percentVar <- pca$sdev ^ 2 / sum(cv)
 
     pcs <- pca$x
     attr(pcs, "percentVar") <- percentVar
