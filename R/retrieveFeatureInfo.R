c8acf1dbf10b22e07d023b6166554f5aa4a648b3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-21 17:11:46 -0700

    Added a feature (meta)data retrieval function. Minor fixes to the cell retrieval function.

--- R/retrieveFeatureInfo.R
+++ R/retrieveFeatureInfo.R
@@ -1,6 +1,6 @@
-#' Cell-based data retrieval
+#' Feature-based data retrieval
 #'
-#' Retrieves a per-cell (meta)data field from a \linkS4class{SingleCellExperiment} based on a single keyword,
+#' Retrieves a per-feature (meta)data field from a \linkS4class{SingleCellExperiment} based on a single keyword,
 #' typically for use in visualization functions.
 #' 
 #' @param x A \linkS4class{SingleCellExperiment} object.
@@ -10,7 +10,7 @@
 #' @param assay.type String or integer scalar specifying the assay from which expression values should be extracted.
 #' 
 #' @return A list containing \code{name}, a string with the name of the extracted field (usually identically to \code{by});
-#' and \code{value}, a vector of length equal to \code{ncol(x)} containing per-cell (meta)data values.
+#' and \code{value}, a vector of length equal to \code{ncol(x)} containing per-feature (meta)data values.
 #' If \code{by=NULL} or was not found in \code{x}, both \code{name} and \code{value} are set to \code{NULL}.
 #'
 #' @details
@@ -22,13 +22,11 @@
 #'
 #' Given a character string in \code{by}, this function will:
 #' \enumerate{
-#' \item Search \code{\link{colData}} for a column named \code{by}, 
+#' \item Search \code{\link{rowData}} for a column named \code{by}, 
 #' and return the corresponding field as the output \code{value}.
-#' We do not consider nested elements within the \code{colData}.
-#' \item Search \code{\link{assay}(x, assay.type)} for a row named \code{by}, 
+#' We do not consider nested elements within the \code{rowData}.
+#' \item Search \code{\link{assay}(x, assay.type)} for a column named \code{by}, 
 #' and return the expression vector for this feature as the output \code{value}.
-#' \item Search each alternative experiment in \code{\link{altExps}(x)} for a row names \code{by},
-#' and return the expression vector for this feature at \code{assay.type} as the output \code{value}.
 #' }
 #' Any match will cause the function to return without considering later possibilities.
 #' If there is a name clash that results in retrieval of an unintended field,
@@ -37,14 +35,9 @@
 #'
 #' @author Aaron Lun
 #' @seealso
-#' \code{\link{plotColData}}, 
-#' \code{\link{plotReducedDim}}, 
-#' \code{\link{plotExpression}}, 
-#' \code{\link{plotPlatePosition}},
-#' and most other cell-based plotting functions.
+#' \code{\link{plotRowData}} and other feature-based plotting functions.
 #' 
 #' @examples
-#' ## Set up an example SingleCellExperiment
 #' data("sc_example_counts")
 #' data("sc_example_cell_info")
 #' example_sce <- SingleCellExperiment(
@@ -52,34 +45,35 @@
 #'     colData = sc_example_cell_info
 #' )
 #' example_sce <- normalize(example_sce)
+#' rowData(example_sce)$blah <- sample(LETTERS,
+#'     nrow(example_sce), replace=TRUE)
 #'
-#' retrieveCellInfo(example_sce, "Cell_Cycle")
-#' retrieveCellInfo(example_sce, "Gene_0001")
+#' str(retrieveFeatureInfo(example_sce, "blah"))
+#' str(retrieveFeatureInfo(example_sce, "Cell_001"))
 #'
-#' arbitrary.field <- rnorm(ncol(example_sce))
-#' retrieveCellInfo(example_sce, I(arbitrary.field))
-#' retrieveCellInfo(example_sce, data.frame(stuff=arbitrary.field))
+#' arbitrary.field <- rnorm(nrow(example_sce))
+#' str(retrieveFeatureInfo(example_sce, I(arbitrary.field)))
+#' str(retrieveFeatureInfo(example_sce, data.frame(stuff=arbitrary.field)))
 #'
 #' @export
-#' @importFrom SingleCellExperiment altExp altExpNames
-#' @importFrom SummarizedExperiment colData assay
-retrieveCellInfo <- function(x, by, search=c("colData", "assays", "altExps"), assay.type="logcounts")
+#' @importFrom SummarizedExperiment rowData assay
+retrieveFeatureInfo <- function(x, by, search=c("rowData", "assays"), assay.type="logcounts")
 {
     .mopUp <- function(name, value) {
         list(name=name, value=value)
     } 
 
     if (is(by, "AsIs")) {
-        if (length(by) != ncol(x)) {
-            stop("length of 'AsIs' input should be equal to 'ncol(x)'")
+        if (length(by) != nrow(x)) {
+            stop("length of 'AsIs' input should be equal to 'nrow(x)'")
         }
         return(.mopUp("", as.vector(by)))
     } else if (is.data.frame(by) || is(by, "DataFrame")) {
         if (ncol(by) != 1L) {
             stop("input data frame should only have one column")
         } 
-        if (nrow(by) != ncol(x)) {
-            stop("number of rows of input data frame should be equal to 'ncol(x)'")
+        if (nrow(by) != nrow(x)) {
+            stop("number of rows of input data frame should be equal to 'nrow(x)'")
         }
         return(.mopUp(colnames(by)[1], by[,1]))
     }
@@ -92,40 +86,19 @@ retrieveCellInfo <- function(x, by, search=c("colData", "assays", "altExps"), as
         search <- match.arg(search, several.ok=TRUE)
     }
 
-    if ("colData" %in% search) {
-        cd <- colData(x)
+    if ("rowData" %in% search) {
+        cd <- rowData(x)
         if (by %in% colnames(cd)) {
             return(.mopUp(by, cd[,by]))
         }
     }
 
     if ("assays" %in% search) {
-        m <- match(by, rownames(x))
+        m <- match(by, colnames(x))
         if (!is.na(m)) {
-            return(.mopUp(by, assay(x, assay.type, withDimnames=FALSE)[m,]))
-        }
-    }
-
-    if ("altExps" %in% search) {
-        for (i in seq_along(altExpNames(x))) {
-            current <- altExp(x, i)
-            m <- match(by, rownames(current))
-            if (!is.na(m)) {
-                return(.mopUp(by, assay(current, assay.type, withDimnames=FALSE)[m,]))
-            }
+            return(.mopUp(by, assay(x, assay.type, withDimnames=FALSE)[,m]))
         }
     }
 
     .mopUp(NULL, NULL)
 }
-
-.coerce_to_factor <- function(x, level.limit, msg) {
-    if (!is.null(x)) {
-        x <- as.factor(x)
-        if (nlevels(x) > level.limit) {
-            stop(sprintf("more than %i levels for '%s'", level.limit, msg))
-        }
-    }
-    x
-}
-
