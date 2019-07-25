fec9b3c8101120e22b9fe0afe559c2f1811eb3db
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-24 21:14:40 -0700

    Stripped out unnecessary tests, updated the remainders.

--- tests/testthat/test-plot-utils.R
+++ tests/testthat/test-plot-utils.R
@@ -3,103 +3,80 @@
 
 example_sce <- normed
 
-test_that("visualization variable picker works for columns with unnamed strings", {
-    # Known metadata.
-    out <- scater:::.choose_vis_values(example_sce, "Mutation_Status", mode="column")
+altExp(example_sce) <- normed[1:10,]
+rownames(altExp(example_sce)) <- paste0(rownames(altExp(example_sce)), ".0")
+
+altExp(example_sce, 2) <- normed[1:5,]
+rownames(altExp(example_sce, 2)) <- paste0(rownames(altExp(example_sce, 2)), "-R")
+
+test_that("retrieveCellInfo works in the basic case", {
+    out <- retrieveCellInfo(example_sce, "Mutation_Status")
     expect_identical(out$val, example_sce$Mutation_Status)
     expect_identical(out$name, "Mutation_Status")
 
-    out <- scater:::.choose_vis_values(example_sce, "Cell_Cycle", mode="column")
+    out <- retrieveCellInfo(example_sce, "Cell_Cycle")
     expect_identical(out$val, example_sce$Cell_Cycle)
     expect_identical(out$name, "Cell_Cycle")
 
     # Known gene exprs. 
-    out <- scater:::.choose_vis_values(example_sce, "Gene_0001", mode="column")
-    expect_identical(out$val, logcounts(example_sce)["Gene_0001",])
+    out <- retrieveCellInfo(example_sce, "Gene_0001")
+    expect_identical(out$val, unname(logcounts(example_sce)["Gene_0001",]))
     expect_identical(out$name, "Gene_0001")
 
-    out <- scater:::.choose_vis_values(example_sce, "Gene_0100", mode="column")
-    expect_identical(out$val, logcounts(example_sce)["Gene_0100",])
+    out <- retrieveCellInfo(example_sce, "Gene_0100")
+    expect_identical(out$val, unname(logcounts(example_sce)["Gene_0100",]))
     expect_identical(out$name, "Gene_0100")
 
+    # Alternative experiments.
+    out <- retrieveCellInfo(example_sce, "Gene_0005.0")
+    expect_identical(out$val, unname(logcounts(altExp(example_sce))["Gene_0005.0",]))
+    expect_identical(out$name, "Gene_0005.0")
+
+    out <- retrieveCellInfo(example_sce, "Gene_0005-R")
+    expect_identical(out$val, unname(logcounts(altExp(example_sce, 2))["Gene_0005-R",]))
+    expect_identical(out$name, "Gene_0005-R")
+
     # Known not to be either.
-    expect_error(scater:::.choose_vis_values(example_sce, "WHEE", mode="column"), "cannot find .* in any fields")
-    expect_error(scater:::.choose_vis_values(example_sce, "Mutation_Status", mode="column", search = "exprs"), "cannot find .* in exprs")
-    expect_error(scater:::.choose_vis_values(example_sce, "Gene_0001", mode="column", search = "metadata"), "cannot find .* in metadata")
+    expect_error(retrieveCellInfo(example_sce, "WHEE"), "cannot find")
+    expect_error(retrieveCellInfo(example_sce, "Mutation_Status", search = "assays"), "cannot find")
+    expect_error(retrieveCellInfo(example_sce, "Gene_0002", search = "colData"), "cannot find")
+})
 
-    # Responsive to search mode.
-    expect_error(scater:::.choose_vis_values(example_sce, "Gene_0002", mode="column", search = "metadata"), "cannot find")
-
+test_that("retrieveCellInfo handles clashes correctly", {
     example_sce$Gene_0002 <- seq_len(ncol(example_sce))
-    out_m <- scater:::.choose_vis_values(example_sce, "Gene_0002", mode="column", search = "metadata")
+    out_m <- retrieveCellInfo(example_sce, "Gene_0002", search = "colData")
     expect_identical(out_m$val, example_sce$Gene_0002)
     expect_identical(out_m$name, "Gene_0002")
 
-    out_f <- scater:::.choose_vis_values(example_sce, "Gene_0002", mode="column", search = "exprs")
-    expect_identical(out_f$val, logcounts(example_sce)["Gene_0002",])
+    out_f <- retrieveCellInfo(example_sce, "Gene_0002", search = "assays")
+    expect_identical(out_f$val, unname(logcounts(example_sce)["Gene_0002",]))
     expect_identical(out_f$name, "Gene_0002")
 
-    out_a <- scater:::.choose_vis_values(example_sce, "Gene_0002", mode="column", search = "any")
-    expect_identical(out_a, out_m)
+    # Respects ordering of inputs.
+    expect_identical(out_f, retrieveCellInfo(example_sce, "Gene_0002", search = c("assays", "colData")))
+    expect_identical(out_m, retrieveCellInfo(example_sce, "Gene_0002", search = c("colData", "assays")))
 })
 
-test_that("visualization variable picker works for columns with named strings", {
-    example_sce$Gene_0002 <- seq_len(ncol(example_sce))
-
-    out <- scater:::.choose_vis_values(example_sce, setNames("Gene_0002", "metadata"), mode="column")
-    expect_identical(out$val, example_sce$Gene_0002)
-    expect_identical(out$name, "Gene_0002")
-
-    out <- scater:::.choose_vis_values(example_sce, setNames("Gene_0002", "exprs"), mode="column")
-    expect_identical(out$val, logcounts(example_sce)["Gene_0002",])
-    expect_identical(out$name, "Gene_0002")
-
-    # The 'search' specifier will override any naming.
-    out <- scater:::.choose_vis_values(example_sce, setNames("Gene_0002", "exprs"), mode="column", search="metadata") 
-    expect_identical(out$val, example_sce$Gene_0002)
-    expect_identical(out$name, "Gene_0002")
-
-    out <- scater:::.choose_vis_values(example_sce, setNames("Gene_0002", "metadata"), mode="column", search="exprs") 
-    expect_identical(out$val, logcounts(example_sce)["Gene_0002",])
-    expect_identical(out$name, "Gene_0002")
-})
-
-test_that("visualization variable picker works fo columns with nested strings", {
-    example_sce$blah <- DataFrame(X=runif(ncol(example_sce)), Y=sample(LETTERS, ncol(example_sce), replace=TRUE))
-    out <- scater:::.choose_vis_values(example_sce, c("blah", "X"), mode="column", search="any")
-    expect_identical(out$val, example_sce$blah$X)
-    expect_identical(out$name, "blah:X")
-
-    out <- scater:::.choose_vis_values(example_sce, c("blah", "Y"), mode="column", search="metadata")
-    expect_identical(out$val, example_sce$blah$Y)
-    expect_identical(out$name, "blah:Y")
-    
-    # Check error condition with search='exprs'.
-    expect_error(scater:::.choose_vis_values(example_sce, c("blah", "Y"), mode="column", search="exprs"), "character vector")
-
-    # Works for internal fields.
-    sizeFactors(example_sce) <- runif(ncol(example_sce))
-    out <- scater:::.choose_vis_values(example_sce, c(NA, "size_factor"), mode="column", search="any")
-    expect_identical(out$val, sizeFactors(example_sce))
-    expect_identical(out$name, "size_factor")
-
-    out <- scater:::.choose_vis_values(example_sce, c(NA, "size_factor"), mode="column", search="metadata")
-    expect_identical(out$val, sizeFactors(example_sce))
-    expect_identical(out$name, "size_factor")
-
-    expect_error(scater:::.choose_vis_values(example_sce, "size_factor", mode="column", search="any"), "cannot find .* in any fields")
-})
-
-test_that("visualization variable picker works for columns with data.frames", {
+test_that("retrieveCellInfo handles wrapped elements and columns with data.frames", {
     thing <- data.frame(B=runif(ncol(example_sce)))
-    out <- scater:::.choose_vis_values(example_sce, thing, mode = "column")
+    out <- retrieveCellInfo(example_sce, thing)
     expect_identical(out$val, thing$B)
     expect_identical(out$name, "B")
 
+    thing <- DataFrame(B=runif(ncol(example_sce)))
+    out <- retrieveCellInfo(example_sce, thing)
+    expect_identical(out$val, thing$B)
+    expect_identical(out$name, "B")
+
+    thing <- I(runif(ncol(example_sce)))
+    out <- retrieveCellInfo(example_sce, thing)
+    expect_identical(out$val, as.numeric(thing))
+    expect_identical(out$name, "")
+
     # Check errors.
     rething <- data.frame(B=runif(ncol(example_sce)), C=2)
-    expect_error(scater:::.choose_vis_values(example_sce, rething, mode = "column"), "one column")
-    expect_error(scater:::.choose_vis_values(example_sce, thing[1:10,,drop=FALSE], mode = "column"), "number of rows")
+    expect_error(retrieveCellInfo(example_sce, rething), "one column")
+    expect_error(retrieveCellInfo(example_sce, rething[1:10,1,drop=FALSE]), "number of rows")
 })
 
 ###################################################
@@ -107,141 +84,62 @@ test_that("visualization variable picker works for columns with data.frames", {
 set.seed(1312313)
 rowData(example_sce) <- DataFrame(HAPPY=runif(nrow(example_sce)), SAD=rbinom(nrow(example_sce), 1, 0.5)==1)
 
-test_that("visualization variable picker works for rows with strings", {
+test_that("retrieveFeatureInfo works for rows with strings", {
     # Known metadata.
-    out <- scater:::.choose_vis_values(example_sce, "HAPPY", mode="row")
+    out <- retrieveFeatureInfo(example_sce, "HAPPY")
     expect_identical(out$val, rowData(example_sce)$HAPPY)
     expect_identical(out$name, "HAPPY")
 
-    out <- scater:::.choose_vis_values(example_sce, "SAD", mode="row")
+    out <- retrieveFeatureInfo(example_sce, "SAD")
     expect_identical(out$val, rowData(example_sce)$SAD)
     expect_identical(out$name, "SAD")
 
     # Known exprs.
-    out <- scater:::.choose_vis_values(example_sce, "Cell_001", mode="row")
-    expect_identical(out$val, logcounts(example_sce)[,"Cell_001"])
+    out <- retrieveFeatureInfo(example_sce, "Cell_001")
+    expect_identical(out$val, unname(logcounts(example_sce)[,"Cell_001"]))
     expect_identical(out$name, "Cell_001")
 
-    out <- scater:::.choose_vis_values(example_sce, "Cell_010", mode="row")
-    expect_identical(out$val, logcounts(example_sce)[,"Cell_010"])
+    out <- retrieveFeatureInfo(example_sce, "Cell_010")
+    expect_identical(out$val, unname(logcounts(example_sce)[,"Cell_010"]))
     expect_identical(out$name, "Cell_010")
 
     # Handles errors properly.
-    expect_error(scater:::.choose_vis_values(example_sce, "whee", mode = "row", search = "any"), "cannot find .* in any fields")
-    expect_error(scater:::.choose_vis_values(example_sce, "Mutation_Status", mode = "row", search = "exprs"), "cannot find .* in exprs")
-    expect_error(scater:::.choose_vis_values(example_sce, "Gene_0001", mode = "row", search = "metadata"), "cannot find .* in metadata")
+    expect_error(retrieveFeatureInfo(example_sce, "whee"), "cannot find")
+    expect_error(retrieveFeatureInfo(example_sce, "HAPPY", search = "assays"), "cannot find")
+    expect_error(retrieveFeatureInfo(example_sce, "Cell_010", search = "rowData"), "cannot find")
+})
 
-    # Responsive to search mode.
-    expect_error(scater:::.choose_vis_values(example_sce, "Cell_002", mode="row", search = "metadata"), "cannot find")
-
+test_that("retrieveFeatureInfo is responsive to search mode", {
     rowData(example_sce)$Cell_002 <- seq_len(nrow(example_sce))
-    out_m <- scater:::.choose_vis_values(example_sce, "Cell_002", mode="row", search = "metadata")
+    out_m <- retrieveFeatureInfo(example_sce, "Cell_002", search = "rowData")
     expect_identical(out_m$val, rowData(example_sce)$Cell_002)
     expect_identical(out_m$name, "Cell_002")
 
-    out_f <- scater:::.choose_vis_values(example_sce, "Cell_002", mode="row", search = "exprs")
-    expect_identical(out_f$val, logcounts(example_sce)[,"Cell_002"])
+    out_f <- retrieveFeatureInfo(example_sce, "Cell_002", , search = "assays")
+    expect_identical(out_f$val, unname(logcounts(example_sce)[,"Cell_002"]))
     expect_identical(out_f$name, "Cell_002")
 
-    out_a <- scater:::.choose_vis_values(example_sce, "Cell_002", mode="row", search = "any")
-    expect_identical(out_a, out_m)
-})
-
-test_that("visualization variable picker works for rows with named strings", {
-    rowData(example_sce)$Cell_002 <- seq_len(nrow(example_sce))
-
-    out <- scater:::.choose_vis_values(example_sce, setNames("Cell_002", "metadata"), mode="row")
-    expect_identical(out$val, rowData(example_sce)$Cell_002)
-    expect_identical(out$name, "Cell_002")
-
-    out <- scater:::.choose_vis_values(example_sce, setNames("Cell_002", "exprs"), mode="row")
-    expect_identical(out$val, logcounts(example_sce)[,"Cell_002"])
-    expect_identical(out$name, "Cell_002")
-
-    # Overriden by 'search'.
-    out <- scater:::.choose_vis_values(example_sce, setNames("Cell_002", "exprs"), mode="row", search="metadata") 
-    expect_identical(out$val, rowData(example_sce)$Cell_002)
-    expect_identical(out$name, "Cell_002")
-
-    out <- scater:::.choose_vis_values(example_sce, setNames("Cell_002", "metadata"), mode="row", search="exprs") 
-    expect_identical(out$val, logcounts(example_sce)[,"Cell_002"])
-    expect_identical(out$name, "Cell_002")
-})
-
-test_that("visualization variable picker works for rows with nested strings", {
-    rowData(example_sce)$blah <- DataFrame(X=runif(nrow(example_sce)), Y=sample(LETTERS, nrow(example_sce), replace=TRUE))
-    out <- scater:::.choose_vis_values(example_sce, c("blah", "X"), mode="row", search="any")
-    expect_identical(out$val, rowData(example_sce)$blah$X)
-    expect_identical(out$name, "blah:X")
-
-    out <- scater:::.choose_vis_values(example_sce, c("blah", "Y"), mode="row", search="metadata")
-    expect_identical(out$val, rowData(example_sce)$blah$Y)
-    expect_identical(out$name, "blah:Y")
-    
-    # Throws errors if search='exprs'.
-    expect_error(scater:::.choose_vis_values(example_sce, c("blah", "Y"), mode="row", search="exprs"), "character vector")
-
-    # Works for internal fields.
-    isSpike(example_sce, "ERCC") <- 1:10
-    out <- scater:::.choose_vis_values(example_sce, c(NA, "is_spike_ERCC"), mode="row", search="any")
-    expect_identical(out$val, isSpike(example_sce, "ERCC"))
-    expect_identical(out$name, "is_spike_ERCC")
-
-    out <- scater:::.choose_vis_values(example_sce, c(NA, "is_spike"), mode="row", search="metadata")
-    expect_identical(out$val, isSpike(example_sce))
-    expect_identical(out$name, "is_spike")
-
-    expect_error(scater:::.choose_vis_values(example_sce, "is_spike_ERCC", mode="row", search="any"), "cannot find .* in any fields")
+    expect_identical(out_m, retrieveFeatureInfo(example_sce, "Cell_002", search=c("rowData", "assays")))
+    expect_identical(out_f, retrieveFeatureInfo(example_sce, "Cell_002", search=c("assays", "rowData")))
 })
      
-test_that("visualization variable picker works for rows with data.frames", {
+test_that("retrieveFeatureInfo works for rows with data.frames", {
     thing <- data.frame(B=runif(nrow(example_sce)))
-    out <- scater:::.choose_vis_values(example_sce, thing, mode = "row")
+    out <- retrieveFeatureInfo(example_sce, thing, )
     expect_identical(out$val, thing$B)
     expect_identical(out$name, "B")
 
+    thing <- DataFrame(B=runif(nrow(example_sce)))
+    out <- retrieveFeatureInfo(example_sce, thing)
+    expect_identical(out$val, thing$B)
+    expect_identical(out$name, "B")
+
+    thing <- I(runif(nrow(example_sce)))
+    out <- retrieveFeatureInfo(example_sce, thing)
+    expect_identical(out$val, as.numeric(thing))
+    expect_identical(out$name, "")
+
     rething <- data.frame(B=runif(ncol(example_sce)), C=2)
-    expect_error(scater:::.choose_vis_values(example_sce, rething, mode = "row"), "one column")
-    expect_error(scater:::.choose_vis_values(example_sce, thing[1:10,,drop=FALSE], mode = "row"), "number of rows")
-})
-
-###################################################
-
-test_that("visualization variable picker discards one-level factors if requested", {
-    example_sce$BLAH <- 0
-    out <- scater:::.choose_vis_values(example_sce, "BLAH", mode="column")
-    expect_identical(out$val, example_sce$BLAH)
-    expect_identical(out$name, "BLAH")
-                 
-    out <- scater:::.choose_vis_values(example_sce, "BLAH", mode="column", discard_solo = TRUE)
-    expect_identical(out$val, NULL)
-    expect_identical(out$name, NULL) 
-})
-
-test_that("visualization variable picker is responsive to exprs_values", {
-    out <- scater:::.choose_vis_values(example_sce, "Gene_0001", mode="column", exprs_values="counts")
-    expect_identical(out$val, counts(example_sce)["Gene_0001",])
-    expect_identical(out$name, "Gene_0001")
-
-    out <- scater:::.choose_vis_values(example_sce, "Cell_001", mode="row", exprs_values="counts")
-    expect_identical(out$val, counts(example_sce)[,"Cell_001"])
-    expect_identical(out$name, "Cell_001")
-})
-
-test_that("visualization variable picker correctly coerces into factors", {
-    example_sce$BLAH <- rep(LETTERS, length.out=ncol(example_sce))
-
-    out <- scater:::.choose_vis_values(example_sce, "BLAH", mode="column")
-    expect_identical(out$val, example_sce$BLAH)
-    expect_identical(out$name, "BLAH")
-
-    out2 <- scater:::.choose_vis_values(example_sce, "BLAH", mode="column", coerce_factor = TRUE)
-    expect_identical(out2$val, as.factor(out$val))                 
-    expect_identical(out2$name, "BLAH")
-
-    expect_error(scater:::.choose_vis_values(example_sce, "BLAH", mode="column", coerce_factor = TRUE, level_limit = 10), 
-                 "number of unique levels")
-
-    out3 <- scater:::.choose_vis_values(example_sce, "BLAH", mode="column", coerce_factor = TRUE, level_limit = 100)
-    expect_identical(out2, out3)
+    expect_error(retrieveFeatureInfo(example_sce, rething, ), "one column")
+    expect_error(retrieveFeatureInfo(example_sce, rething[1:10,1,drop=FALSE], ), "number of rows")
 })
--- tests/testthat/test-qc-plot.R
+++ tests/testthat/test-qc-plot.R
@@ -7,84 +7,22 @@ wt_qc <- calculateQCMetrics(wo_qc,
     feature_controls = list(set1 = 1:500),
     cell_controls = list(whee = 1:10))
 
-wt_qc_compact <- calculateQCMetrics(wo_qc, 
-    feature_controls = list(set1 = 1:500),
-    cell_controls = list(whee = 1:10),
-    compact=TRUE)
-
 library(Matrix)
 sparsified <- wt_qc
 counts(sparsified) <- as(counts(sparsified), "dgCMatrix")
 
 #######################################################################
 
-test_that("the QC hunter works as expected on columns", {
-    for (x in c("total_counts", "total_features_by_counts",
-                "total_counts_endogenous", "total_counts_feature_control",
-                "total_counts_set1", "pct_counts_set1")) {
-
-        expect_error(scater:::.qc_hunter(wo_qc, x, mode = "column"), "failed")
-        expect_warning(out <- scater:::.qc_hunter(wo_qc, x, mode = "column", error = FALSE), "failed")
-        expect_identical(out, NULL)
-        expect_identical(scater:::.qc_hunter(wt_qc, x, mode = "column"), x)
-    }
-
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_counts", mode = "column"), 
-                     c("scater_qc", "all", "total_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_features_by_counts", mode = "column"), 
-                     c("scater_qc", "all", "total_features_by_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_counts_endogenous", mode = "column"), 
-                     c("scater_qc", "endogenous", "total_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_counts_feature_control", mode = "column"), 
-                     c("scater_qc", "feature_control", "total_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_counts_set1", mode = "column"), 
-                     c("scater_qc", "feature_control_set1", "total_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "pct_counts_set1", mode = "column"), 
-                     c("scater_qc", "feature_control_set1", "pct_counts"))
-})
-
-test_that("the QC hunter works as expected on rows", {
-    for (x in c("total_counts", "n_cells_by_counts",
-                "total_counts_non_control", "total_counts_cell_control",
-                "total_counts_whee", "pct_counts_whee")) {
-
-        expect_error(scater:::.qc_hunter(wo_qc, x, mode = "row"), "failed")
-        expect_warning(out <- scater:::.qc_hunter(wo_qc, x, mode = "row", error = FALSE), "failed")
-        expect_identical(out, NULL)
-        expect_identical(scater:::.qc_hunter(wt_qc, x, mode = "row"), x)
-    }
-
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_counts", mode = "row"), 
-                     c("scater_qc", "all", "total_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "n_cells_by_counts", mode = "row"), 
-                     c("scater_qc", "all", "n_cells_by_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_counts_non_control", mode = "row"), 
-                     c("scater_qc", "non_control", "total_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_counts_cell_control", mode = "row"), 
-                     c("scater_qc", "cell_control", "total_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "total_counts_whee", mode = "row"), 
-                     c("scater_qc", "cell_control_whee", "total_counts"))
-    expect_identical(scater:::.qc_hunter(wt_qc_compact, "pct_counts_whee", mode = "row"), 
-                     c("scater_qc", "cell_control_whee", "pct_counts"))
-})
-
-#######################################################################
-
 test_that("plotHighestExprs works on vanilla cases", {
     expect_s3_class(plotHighestExprs(wt_qc), "ggplot")
-    expect_s3_class(plotHighestExprs(wt_qc_compact), "ggplot")
+    expect_s3_class(plotHighestExprs(wt_qc), "ggplot")
     expect_s3_class(plotHighestExprs(wt_qc, as_percentage = FALSE), "ggplot")
 })
 
 test_that("plotHighestExprs' aesthetics choices work", {
-    expect_s3_class(plotHighestExprs(wt_qc_compact, controls = c("scater_qc", "is_feature_control_set1")), "ggplot")
-    expect_s3_class(plotHighestExprs(wt_qc_compact, colour_cells_by = c("scater_qc", "all", "total_counts")), "ggplot")
-
+    expect_s3_class(plotHighestExprs(wt_qc, colour_cells_by = c("total_counts")), "ggplot")
     expect_s3_class(plotHighestExprs(wt_qc, colour_cells_by = "Mutation_Status"), "ggplot")
     expect_s3_class(plotHighestExprs(wt_qc, colour_cells_by = NULL), "ggplot")
-    expect_s3_class(plotHighestExprs(wt_qc, controls = "is_feature_control_set1"), "ggplot")
-    expect_s3_class(plotHighestExprs(wt_qc, controls = NULL), "ggplot")
-    
     expect_s3_class(plotHighestExprs(wt_qc, colour_cells_by = "Gene_0001", by_exprs_values = "counts"), "ggplot")
 
     # Responds to by_show_single.
@@ -94,9 +32,7 @@ test_that("plotHighestExprs' aesthetics choices work", {
     expect_s3_class(plotHighestExprs(dummy, colour_cells_by = "whee", by_show_single = FALSE), "ggplot")
 
     # Checking out the error messages when values are missing.
-    expect_error(plotHighestExprs(wo_qc, colour_cells_by = NULL), "failed to find")
-    expect_error(plotHighestExprs(wo_qc, controls = NULL), "failed to find")
-    expect_s3_class(plotHighestExprs(wo_qc, controls = NULL, colour_cells_by = NULL), "ggplot")
+    plotHighestExprs(wo_qc, colour_cells_by = NULL)
 })
 
 test_that("plotHighestExprs works with different feature selections", {
@@ -128,44 +64,8 @@ test_that("plotHighestExprs works with different feature selections", {
 test_that("plotHighestExprs works on alternative exprs", {
     alt_sce <- wt_qc 
     assayNames(alt_sce) <- "whee"
-    expect_error(plotHighestExprs(alt_sce, exprs_values="whee"), "failed to find")
-    alt_sce$total_features_by_whee <- runif(ncol(alt_sce))
     expect_s3_class(plotHighestExprs(alt_sce, exprs_values="whee"), "ggplot")
 
     # Works for sparse matrices.
     expect_s3_class(plotHighestExprs(sparsified), "ggplot")
-})
-
-#######################################################################
-
-test_that("plotExprsFreqVsMean works as expected", {
-    expect_s3_class(plotExprsFreqVsMean(wt_qc), "ggplot")
-
-    # Checking arguments are passed to plotRowData.
-    expect_s3_class(plotExprsFreqVsMean(wt_qc, add_legend = FALSE), "ggplot")
-    expect_s3_class(plotExprsFreqVsMean(wt_qc, size_by = 'n_cells_by_counts'), "ggplot")
-
-    # Checking we can turn off control colouring and other settings.
-    expect_s3_class(plotExprsFreqVsMean(wt_qc, controls=NULL), "ggplot")
-    expect_s3_class(plotExprsFreqVsMean(wt_qc, show_smooth=FALSE), "ggplot")
-    expect_s3_class(plotExprsFreqVsMean(wt_qc, show_se=FALSE), "ggplot")
-
-    # Avoid computing metrics if there are no controls.
-    rowData(wt_qc)$is_feature_control <- FALSE
-    expect_s3_class(plotExprsFreqVsMean(wt_qc), "ggplot")
-
-    # Recognizes alternative exprs_values.
-    alt_sce <- wt_qc 
-    assayNames(alt_sce) <- "whee"
-    expect_error(plotExprsFreqVsMean(alt_sce, exprs_values="whee"), "failed to find")
-    rowData(alt_sce)$n_cells_by_whee <- runif(ncol(alt_sce))
-    rowData(alt_sce)$mean_whee <- runif(ncol(alt_sce))
-    expect_s3_class(plotExprsFreqVsMean(alt_sce, exprs_values="whee"), "ggplot")
-
-    # Checking errors.
-    rowData(wo_qc)$whee <- runif(nrow(wo_qc))
-    rowData(wo_qc)$stuff <- runif(nrow(wo_qc))
-    expect_error(plotExprsFreqVsMean(wo_qc), "failed to find")
-    expect_error(plotExprsFreqVsMean(wo_qc, freq_exprs="whee"), "failed to find")
-    expect_s3_class(plotExprsFreqVsMean(wo_qc, freq_exprs="whee", mean_exprs="stuff", controls=NULL), "ggplot")
 })
