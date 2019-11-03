09b1deb46fb855b2256e930a5534cb2bc93f817c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-02 20:15:57 -0700

    Cleaned out unnecessary data scripts.

--- data/sc_example_cell_info.R
+++
@@ -1,8 +0,0 @@
-.Deprecated(msg="'data(sc_example_cell_info)' is deprecated.\nUse mockSCE() instead.")
-sc_example_cell_info <- (function() {
-    bfc <- BiocFileCache::BiocFileCache(ask=FALSE)
-    bpath <- BiocFileCache::bfcrpath(bfc, "https://github.com/davismcc/scater/raw/a3d87aa61ebc050374e2ab1ec29a428d39744281/data/sc_example_cell_info.RData")
-    env <- new.env()
-    load(bpath, envir=env)
-    env$sc_example_cell_info
-})()
