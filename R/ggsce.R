e92cbc14ba3b4b8e2b2ef8178240e08d48c4beca
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-19 13:27:58 -0800

    Flipped makePerFeatureDF to use lazy evaluation scheme. Renamed gg functions to refer to each dimension. Docfixes, updated vignette for new function names.

--- R/ggsce.R
+++ R/ggsce.R
@@ -10,27 +10,52 @@
 #' @param ... Further arguments to pass to \link{ggplot}.
 #' 
 #' @details
-#' This function generates a data.frame from the contents of a \linkS4class{SingleCellExperiment} and passes it to \code{\link{ggplot}}.
-#' Almost any field in the \code{x} can be referenced in subsequent \pkg{ggplot2} commands.
-#' 
+#' These functions generate a data.frame from the contents of a \linkS4class{SingleCellExperiment} and pass it to \code{\link{ggplot}}.
+#' Almost any row or column or metadata field in the \code{x} can be referenced in subsequent \pkg{ggplot2} commands.
+#'
+#' \code{ggcells} treats cells as the data values,
+#' so users can reference row names of \code{x}, column metadata variables and dimensionality reduction results.
+#' They can also reference row names and metadata variables for alternative Experiments.
+#'
+#' \code{ggfeatures} treats features as the data values,
+#' so users can reference column names of \code{x} and row metadata variables.
+#'
 #' @return
 #' A \link{ggplot} object containing (almost) everything in \code{x}.
 #'
 #' @author Aaron Lun
 #'
 #' @seealso
-#' \code{\link{makePerCellDF}}, for the construction of the data.frame.
+#' \code{\link{makePerCellDF}} and \code{\link{makePerFeatureDF}}, for the construction of the data.frame.
 #'
 #' @examples
 #' example_sce <- mockSCE()
 #' example_sce <- logNormCounts(example_sce)
 #' example_sce <- runPCA(example_sce)
 #'
-#' ggsce(example_sce) + geom_point(aes(x=PCA.1, y=PCA.2, color=Gene_0001))
+#' ggcells(example_sce) + 
+#'     geom_point(aes(x=PCA.1, y=PCA.2, color=Gene_0001))
+#'
+#' ggcells(example_sce) + 
+#'     geom_violin(aes(x=Mutation_Status, y=Gene_0001)) +
+#'     facet_wrap(~Cell_Cycle)
+#'
+#' rowData(example_sce)$GC <- runif(nrow(example_sce))
+#' ggfeatures(example_sce, mapping=aes(x=GC, y=Cell_001)) + 
+#'     geom_point() +
+#'     stat_smooth()
 #'
 #' @export
 #' @importFrom ggplot2 ggplot
-ggsce <- function(x, exprs_values="logcounts", use_altexps=FALSE, ...) {
+#' @rdname ggsce
+ggcells <- function(x, exprs_values="logcounts", use_altexps=FALSE, ...) {
     df <- makePerCellDF(x, exprs_values=exprs_values, use_altexps=use_altexps)
+    ggplot(df, ...)
+}
+
+#' @export
+#' @rdname ggsce
+ggfeatures <- function(x, exprs_values="logcounts", ...) {
+    df <- makePerFeatureDF(x, exprs_values=exprs_values)
     ggplot(df, ...)
 }
--- R/makePerFeatureDF.R
+++ R/makePerFeatureDF.R
@@ -1,59 +1,70 @@
-#' Create a per-cell data.frame 
+#' Create a per-feature data.frame from a SingleCellDataFrame
 #'
-#' Create a per-cell data.frame from a \linkS4class{SingleCellExperiment},
+#' Create a per-feature data.frame (i.e., where each row represents a feature) from a \linkS4class{SingleCellExperiment},
 #' most typically for creating custom \pkg{ggplot2} plots.
 #'
 #' @param x A \linkS4class{SingleCellExperiment} object.
-#' @param ... Any number of elements, each following the format of the \code{by} argument in \code{\link{retrieveFeatureInfo}}.
-#' 
-#' Briefly, each element can be a single string, in which case it is assumed to refer to a \code{\link{rowData}} column
-#' or a column of \code{\link{assay}(x, exprs_values)}.
+#' This is expected to have non-\code{NULL} row names.
+#' @param exprs_values String or integer scalar indicating the assay to use to obtain expression values.
+#' Must refer to a matrix-like object with integer or numeric values.
 #'
-#' Alternatively, each element can be an \link{AsIs}-wrapped vector, in which case it is used directly.
-#' In this case, the element is expected to be named in \code{...}.
-#' @param exprs_values String or integer scalar specifying the assay from which expression values should be extracted.
-#'
-#' @return A data.frame containing the requested fields,
-#' named according to the names in \code{...} or the values themselves (if the values are strings and no names are supplied).
-#' Each row corresponds to a feature (i.e., row) of \code{x},
+#' @return A data.frame containing one field per aspect of data in \code{x} - see Details.
+#' Each row corresponds to a feature (i.e., row) of \code{x}.
 #'
 #' @details
-#' This function enables us to conveniently create a data.frame from a \linkS4class{SingleCellExperiment},
-#' ostensibly to put in a \code{ggplot} command.
-#' The user can then use this to create custom plots that are not covered by \code{\link{plotRowData}} and related functions.
+#' This function enables us to conveniently create a per-feature data.frame from a \linkS4class{SingleCellExperiment}.
+#' Each row of the returned data.frame corresponds to a row in \code{x},
+#' while each column of the data.frame corresponds to one aspect of the (meta)data in \code{x}.
+#' Columns are provided in the following order:
+#' \enumerate{
+#' \item Columns named according to \code{colnames(x)} represent the expression values across features for each cell in the \code{exprs_values} assay.
+#' \item Columns named according to the columns of \code{rowData(x)} represent the row metadata variables.
+#' }
+#' Nothing is done to resolve duplicated column names, which will often lead (correctly) to an error in downstream functions like \code{\link{ggplot}}.
 #'
-#' The same \code{exprs_values} is used for all assay-related extractions in \code{...}.
-#' See \code{\link{retrieveFeatureInfo}} for more details.
+#' For the data.frame columns derived from the assays, 
+#' the individual integer or numeric vectors are never actually constructed in the returned data.frame.
+#' Rather, the ALTREP system is used to provide lazy evaluation where vectors are materialized from \code{x} on an as-needed basis.
+#' This allows us to mimic the data.frame structure without materializing the values \emph{en masse},
+#' thus avoiding problems due to loss of sparsity or delays from querying remote sources.
+#' As a result, though, it is probably best to avoid \code{\link{print}}ing or \code{\link{saveRDS}}ing the data.frame or any derivative objects.
 #'
 #' @author Aaron Lun
 #'
 #' @seealso
-#' \code{\link{retrieveFeatureInfo}}, which powers this function.
+#' \code{\link{ggfeatures}}, which uses this function under the hood.
 #'
 #' @examples
 #' example_sce <- mockSCE()
 #' example_sce <- logNormCounts(example_sce)
-#' rowData(example_sce)$stuff <- rnorm(nrow(example_sce))
+#' rowData(example_sce)$Length <- runif(nrow(example_sce))
 #'
-#' df1 <- makePerFeatureDF(example_sce, "stuff", "Cell_001")
-#' head(df1)
-#' 
-#' df2 <- makePerFeatureDF(example_sce,  
-#'     more_stuff="stuff", other_stuff="Cell_002")
-#' head(df2)
-#' 
-#' example_sce <- runPCA(example_sce)
-#' df3 <- makePerFeatureDF(example_sce, 
-#'     blah=I(runif(nrow(example_sce))))
-#' head(df3)
+#' df <- makePerFeatureDF(example_sce)
+#' head(colnames(df))
+#' tail(colnames(df))
 #'
+#' df$Cell_001
+#' df$Length
+#' 
 #' @export
-makePerFeatureDF <- function(x, ..., exprs_values="logcounts") {
-    fields <- .process_free_args(...)
-    for (i in seq_along(fields)) {
-        fields[[i]] <- retrieveFeatureInfo(x, by=fields[[i]], exprs_values = exprs_values)$value
+#' @importFrom SummarizedExperiment assay rowData
+makePerFeatureDF <- function(x, exprs_values="logcounts") {
+    # Collecting the assay values.
+    curmat <- assay(x, exprs_values, withDimnames=FALSE)
+    if (is.null(colnames(x))) {
+        stop("'colnames(x)' cannot be NULL")
     }
-    output <- .create_df_from_list(fields, nrow(x))
-    rownames(output) <- rownames(x)
-    output
+
+    FUNc <- .choose_functions(curmat, get_col=TRUE)
+    assay_vals <- vector("list", ncol(x))
+    for (i in seq_along(assay_vals)) {
+        assay_vals[[i]] <- FUNc(curmat, i)
+    }
+    names(assay_vals) <- colnames(x)
+
+    # Adding row metadata.
+    cbind(
+        data.frame(assay_vals, row.names=rownames(x)),
+        as.data.frame(rowData(x))
+    )
 }
