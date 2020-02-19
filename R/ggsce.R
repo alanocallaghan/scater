15c5b93413588df66ed042229d3d7f697c44cf81
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-18 23:47:58 -0800

    Support data extraction from alternative Experiments.

--- R/ggsce.R
+++ R/ggsce.R
@@ -1,6 +1,6 @@
 #' @export
 #' @importFrom ggplot2 ggplot
-ggsce <- function(x, byrow=FALSE, exprs_values="logcounts", ...) {
+ggsce <- function(x, byrow=FALSE, exprs_values="logcounts", use_altexps=FALSE, ...) {
     if (byrow) {
 
     } else {
@@ -10,41 +10,18 @@ ggsce <- function(x, byrow=FALSE, exprs_values="logcounts", ...) {
 }
 
 #' @export
-#' @importFrom SummarizedExperiment assay colData
 #' @importFrom SingleCellExperiment reducedDim reducedDims 
-sce2dfByColumn <- function(x, exprs_values="logcounts") {
-    output <- list()
+sce2dfByColumn <- function(x, exprs_values="logcounts", use_altexps=FALSE) {
+    output <- list(.harvest_se_by_column(x, exprs_values=exprs_values))
 
-    # Collecting the assay values.
-    curmat <- assay(x, exprs_values, withDimnames=FALSE)
-    if (is.null(rownames(x))) {
-        stop("'rownames(x)' cannot be NULL")
-    }
-
-    if (is.integer(curmat[,0])) {
-        FUNr <- lazy_integer_row
-        FUNc <- lazy_integer_column
-    } else {
-        FUNr <- lazy_double_row
-        FUNc <- lazy_double_column
-    }
-
-    assay_vals <- vector("list", nrow(x))
-    for (i in seq_along(assay_vals)) {
-        assay_vals[[i]] <- FUNr(curmat, i)
-    }
-    names(assay_vals) <- rownames(x)
-    output <- c(output, list(data.frame(assay_vals, row.names=colnames(x))))
-
-    # Adding column metadata.
-    output <- c(output, as.data.frame(colData(x)))
-    
     # Collecting the reduced dimensions.
     all_reds <- reducedDims(x)
     if (length(all_reds)) {
         red_vals <- vector("list", length(all_reds))
+
         for (r in seq_along(red_vals)) {
             curred <- all_reds[[r]]
+            FUNc <- .choose_functions(curred, get_col=TRUE)
 
             splitred <- vector("list", ncol(curred))
             for (i in seq_along(splitred)) {
@@ -60,9 +37,60 @@ sce2dfByColumn <- function(x, exprs_values="logcounts") {
         output <- c(output, list(red_vals))
     }
 
+    # Collecting the alternative Experiments.
+    all_alts <- altExps(x)
+    if (use_altexps && length(all_alts)) {
+        alt_vals <- vector("list", length(all_alts))
+        for (a in seq_along(alt_vals)) {
+            curalt <- .harvest_se_by_column(all_alts[[a]], exprs_values=exprs_values)
+            alt_vals[[a]] <- do.call(cbind, curalt)
+        }
+
+        alt_vals <- unlist(alt_vals, recursive=FALSE)
+        alt_vals <- do.call(data.frame, alt_vals)
+        output <- c(output, list(alt_vals))
+    }
+
     do.call(cbind, output)
 }
 
+.choose_functions <- function(x, get_col=TRUE) {
+    if (is.integer(as.matrix(x[0,0]))) {
+        if (get_col) {
+            lazy_integer_column
+        } else {
+            lazy_integer_row
+        }
+    } else {
+        if (get_col) {
+            lazy_double_column
+        } else {
+            lazy_double_row
+        }
+    }
+}
+
+#' @importFrom SummarizedExperiment assay colData
+.harvest_se_by_column <- function(x, exprs_values) {
+    # Collecting the assay values.
+    curmat <- assay(x, exprs_values, withDimnames=FALSE)
+    if (is.null(rownames(x))) {
+        stop("'rownames(x)' cannot be NULL")
+    }
+
+    FUNr <- .choose_functions(curmat, get_col=FALSE)
+    assay_vals <- vector("list", nrow(x))
+    for (i in seq_along(assay_vals)) {
+        assay_vals[[i]] <- FUNr(curmat, i)
+    }
+    names(assay_vals) <- rownames(x)
+
+    # Adding column metadata.
+    list(
+        data.frame(assay_vals, row.names=colnames(x)),
+        as.data.frame(colData(x))
+    )
+}
 
 # Required for the C++ code.
 getRow <- function(mat, i) mat[i,]
