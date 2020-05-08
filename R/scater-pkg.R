40faff82a4758674dbebfe4aee63be6880c05254
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-07 21:23:14 -0700

    Restored the scater-pkg documentation page.

---
+++ R/scater-pkg.R
@@ -0,0 +1,14 @@
+#' The \pkg{scater} package
+#'
+#' Provides functions for convenient visualization of single-cell data,
+#' mostly via \pkg{ggplot2}.
+#' It also used to provide utilities for data transformation and quality control,
+#' but these have largely been moved to the \pkg{scuttle} package.
+#'
+#' @author Davis McCarthy, Aaron Lun
+#'
+#' @name scater-pkg
+#' @import scuttle
+#' @import methods
+NULL
+
