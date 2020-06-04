95bc2ee12b69a17331ae3296dd53bf444710645f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-06-03 22:13:49 -0700

    Re-export scuttle utilities to ensure scater:: still works.

---
+++ R/reexports.R
@@ -0,0 +1,80 @@
+#' @export
+scuttle::addPerCellQC
+
+#' @export
+scuttle::addPerFeatureQC
+
+#' @export
+scuttle::aggregateAcrossCells
+
+#' @export
+scuttle::aggregateAcrossFeatures
+
+#' @export
+scuttle::calculateAverage
+
+#' @export
+scuttle::calculateCPM
+
+#' @export
+scuttle::calculateFPKM
+
+#' @export
+scuttle::calculateTPM
+
+#' @export
+scuttle::computeLibraryFactors
+
+#' @export
+scuttle::computeMedianFactors
+
+#' @export
+scuttle::isOutlier
+
+#' @export
+scuttle::librarySizeFactors
+
+#' @export
+scuttle::logNormCounts
+
+#' @export
+scuttle::makePerCellDF
+
+#' @export
+scuttle::makePerFeatureDF
+
+#' @export
+scuttle::medianSizeFactors
+
+#' @export
+scuttle::mockSCE
+
+#' @export
+scuttle::normalizeCounts
+
+#' @export
+scuttle::numDetectedAcrossCells
+
+#' @export
+scuttle::numDetectedAcrossFeatures
+
+#' @export
+scuttle::perCellQCMetrics
+
+#' @export
+scuttle::perFeatureQCMetrics
+
+#' @export
+scuttle::quickPerCellQC
+
+#' @export
+scuttle::readSparseCounts
+
+#' @export
+scuttle::sumCountsAcrossCells
+
+#' @export
+scuttle::sumCountsAcrossFeatures
+
+#' @export
+scuttle::uniquifyFeatureNames
