3123d46cb8676378f11c2d60b457e2d013b0ffe3
Aaron Lun <LTLA@users.noreply.github.com>
2020-11-04 10:00:07 -0800

    Support computing and visualization of batch-corrected averages (#128)
    * Added a utility function to compute batch-aware averages.
    * Added a heatmap function for plotting batch-aware group averages.
    * Added tests for the grouped heatmap, moved heatmap tests to a new file.
    * Minor fixes to the group heatmap imports.
    * Added block= support for plotDots.

---
+++ R/plotGroupedHeatmap.R
@@ -0,0 +1,109 @@
+#' Plot heatmap of group-level expression averages
+#'
+#' Create a heatmap of average expression values for each group of cells and specified features in a SingleCellExperiment object.
+#'
+#' @param object A \linkS4class{SingleCellExperiment} object.
+#' @param features A character vector of row names, a logical vector of integer vector of indices specifying rows of \code{object} to show in the heatmap.
+#' @param columns A vector specifying the subset of columns in \code{object} to use when computing averages.
+#' @param exprs_values A string or integer scalar indicating which assay of \code{object} should be used as expression values for colouring in the heatmap.
+#' @param center A logical scalar indicating whether each row should have its mean expression centered at zero prior to plotting. 
+#' @param zlim A numeric vector of length 2, specifying the upper and lower bounds for color mapping of expression values.
+#' Values outside this range are set to the most extreme color.
+#' If \code{NULL}, it defaults to the range of the expression matrix.
+#' @param symmetric A logical scalar specifying whether the default \code{zlim} should be symmetric around zero. 
+#' If \code{TRUE}, the maximum absolute value of \code{zlim} will be computed and multiplied by \code{c(-1, 1)} to redefine \code{zlim}.
+#' @param color A vector of colours specifying the palette to use for mapping expression values to colours. 
+#' This defaults to the default setting in \code{\link[pheatmap]{pheatmap}}.
+#' @param ... Additional arguments to pass to \code{\link[pheatmap]{pheatmap}}.
+#' @param swap_rownames String containing the field of \code{rowData(object)} to be used to 
+#'  identify features instead of \code{rownames(object)} when labelling plot elements.
+#' @param group String specifying the field of \code{\link{colData}(object)} containing the grouping factor, e.g., cell types or clusters.
+#' Alternatively, any value that can be used in the \code{by} argument to \code{\link{retrieveCellInfo}}.
+#' @param block String specifying the field of \code{\link{colData}(object)} containing a blocking factor (e.g., batch of origin).
+#' Alternatively, any value that can be used in the \code{by} argument to \code{\link{retrieveCellInfo}}.
+#'
+#' @details 
+#' This function shows the average expression values for each group of cells on a heatmap, as defined using the \code{group} factor.
+#' A per-group visualization can be preferable to a per-cell visualization when dealing with large number of cells or groups with different size.
+#' If \code{block} is also specified, the block effect is regressed out of the averages with \code{\link{batchCorrectedAverages}} prior to visualization.
+#'
+#' Setting \code{center=TRUE} is useful for examining log-fold changes of each group's expression profile from the average across all groups.
+#' This avoids issues with the entire row appearing a certain colour because the gene is highly/lowly expressed across all cells.
+#'
+#' Setting \code{zlim} preserves the dynamic range of colours in the presence of outliers. 
+#' Otherwise, the plot may be dominated by a few genes, which will \dQuote{flatten} the observed colours for the rest of the heatmap.
+#'
+##' @return A heatmap is produced on the current graphics device. 
+#' The output of \code{\link[pheatmap]{pheatmap}} is invisibly returned.
+#'
+#' @seealso \code{\link[pheatmap]{pheatmap}}, for the underlying function.
+#'
+#' \code{\link{plotHeatmap}}, for a per-cell heatmap.
+#'
+#' @author Aaron Lun
+#' 
+#' @examples
+#' example_sce <- mockSCE()
+#' example_sce <- logNormCounts(example_sce)
+#' example_sce$Group <- paste0(example_sce$Treatment, "+", example_sce$Mutation_Status)
+#'
+#' plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10],
+#'     group="Group")
+#'
+#' plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10],
+#'     group="Group", center=TRUE, symmetric=TRUE)
+#'
+#' plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10],
+#'     group="Group", block="Cell_Cycle", center=TRUE, symmetric=TRUE)
+#'
+#' @export
+#' @importFrom SummarizedExperiment assay 
+#' @importFrom Matrix rowMeans
+#' @importFrom scuttle summarizeAssayByGroup
+plotGroupedHeatmap <- function(object, features, group, block = NULL, columns=NULL, exprs_values = "logcounts", 
+    center = FALSE, zlim = NULL, symmetric = FALSE, color = NULL, swap_rownames=NULL, ...) 
+{
+    # Setting names, otherwise the downstream colouring fails.
+    if (is.null(colnames(object))) { 
+        colnames(object) <- seq_len(ncol(object)) 
+    }
+
+    # Pulling out the features. swap_rownames makes features index a rowdata col
+    feats <- .swap_rownames(object, features, swap_rownames)
+    heat.vals <- assay(object, exprs_values)[feats, , drop=FALSE]
+    rownames(heat.vals) <- features
+    if (!is.null(columns)) {
+        columns <- .subset2index(columns, object, byrow=FALSE)
+        heat.vals <- heat.vals[,columns,drop=FALSE]
+    }
+
+    # Computing aggregates for each group.
+    ids <- DataFrame(group=retrieveCellInfo(object, group, search="colData")$value)
+    if (!is.null(block)) { 
+        ids$block <- retrieveCellInfo(object, block, search="colData")$value
+    }
+    if (!is.null(columns)) {
+        ids <- ids[columns,,drop=FALSE]
+    }
+    heat.se <- summarizeAssayByGroup(heat.vals, ids, statistic="mean")
+    heat.vals <- batchCorrectedAverages(assay(heat.se), group=heat.se$group, block=heat.se$group)
+
+    # Applying centering and all that jazz.
+    if (center) {
+        heat.vals <- heat.vals - rowMeans(heat.vals)
+    }
+    if (is.null(zlim)) {
+        zlim <- range(heat.vals)
+    }
+    if (symmetric) {
+        extreme <- max(abs(zlim))
+        zlim <- c(-extreme, extreme)
+    }
+    if (is.null(color)) {
+        color <- eval(formals(pheatmap::pheatmap)$color, envir=environment(pheatmap::pheatmap))
+    }
+    color.breaks <- seq(zlim[1], zlim[2], length.out=length(color)+1L) 
+
+    # Creating the heatmap as specified.
+    pheatmap::pheatmap(heat.vals, color=color, breaks=color.breaks, ...) 
+}
---
+++ tests/testthat/test-plot-heat.R
@@ -0,0 +1,107 @@
+## Tests for the heatmap-related plotting functions
+## library(scater); library(testthat); source("setup.R"); source("test-plot-heat.R")
+
+example_sce <- normed 
+colData(example_sce) <- cbind(colData(example_sce), perCellQCMetrics(example_sce))
+rowData(example_sce) <- cbind(rowData(example_sce), perFeatureQCMetrics(example_sce))
+rowData(example_sce)$ENS <- gsub("Gene", "ENS", rownames(example_sce))
+rowData(example_sce)$ENS_e1 <- rowData(example_sce)$ENS
+rowData(example_sce)$ENS_e1[1:10] <- NA
+rowData(example_sce)$ENS_e2 <- "constant"
+
+#################################################
+# Testing plotHeatmap
+
+test_that("we can produce heatmaps", {
+    # Testing out the options (need an expect_* clause to avoid skipping).
+    expect_error(plotHeatmap(example_sce, features=rownames(example_sce)[1:10]), NA)
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10], columns = 1:20)
+    plotHeatmap(example_sce, features=rowData(example_sce)[1:10, "ENS"], 
+        swap_rownames = "ENS", columns = 1:20)
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10], exprs_values='counts')
+
+    # Colour parameters for the expression values.
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10], zlim=c(0, 2))
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10], color=viridis::viridis(20))
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10], center=TRUE, symmetric=TRUE)
+         
+    # Testing out the column colouring. 
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10],
+                colour_columns_by=c("Mutation_Status", "Cell_Cycle"))
+
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10],
+                colour_columns_by=c("Mutation_Status", "Gene_0001"), 
+                by_exprs_values = "logcounts")
+
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10],
+                colour_columns_by=list(I(example_sce$Mutation_Status), "Gene_0001"), 
+                by_exprs_values = "logcounts")
+
+    # Testing out the column ordering + colouring. 
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10],
+                order_columns_by=c("Mutation_Status", "Gene_0001"), 
+                by_exprs_values = "logcounts")
+
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10], columns=1:10,
+                order_columns_by=c("Mutation_Status", "Gene_0001"), 
+                by_exprs_values = "logcounts")
+
+    # Testing that column colouring still works when columns have no names.
+    unnamed <- example_sce
+    colnames(unnamed) <- NULL
+    plotHeatmap(unnamed, features=rownames(unnamed)[1:10],
+                colour_columns_by=c("Mutation_Status", "Gene_0001")) 
+
+    # Testing out passing arguments to pheatmap.
+    plotHeatmap(example_sce, features=rownames(example_sce)[1:10], fontsize = 20, legend = FALSE)
+
+    plotHeatmap(example_sce, features = rowData(example_sce)[1:10, "ENS"], 
+        swap_rownames = "ENS", columns = 1:20)
+    expect_error(
+        plotHeatmap(example_sce, features = NA, swap_rownames = "ENS_e1",
+            columns = 1:20),
+        "must have n >= 2 objects to cluster"
+    )
+
+    expect_error(
+        plotHeatmap(example_sce, features = "constant", swap_rownames = "ENS_e2",
+            columns = 1:20),
+        "must have n >= 2 objects to cluster"
+    )
+
+    expect_error(
+        plotHeatmap(example_sce, features = NA, swap_rownames = "sdfsf",
+            columns = 1:20),
+        "Cannot find column sdfsf in rowData"
+    )
+})
+
+#################################################
+# Testing plotGroupedHeatmap
+
+test_that("we can produce grouped heatmaps", {
+    example_sce$Group <- paste0(example_sce$Treatment, "+", example_sce$Mutation_Status)
+
+    # Testing out the options (need an expect_* clause to avoid skipping)
+    expect_error(plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10], group="Group"), NA)
+    plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10], columns=1:20, group="Group")
+    plotGroupedHeatmap(example_sce, features=1:10, group="Group")
+
+    # Works with blocking.
+    plotGroupedHeatmap(example_sce, features=1:10, group="Group", block="Cell_Cycle")
+    plotGroupedHeatmap(example_sce, features=1:10, group="Group", block="Cell_Cycle", columns=20:30)
+
+    # Works with the various color options.
+    plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10],
+        group="Group", center=TRUE, symmetric=TRUE)
+
+    plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10],
+        group="Group", center=TRUE, symmetric=TRUE)
+
+    plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10], group="Group", color=viridis::viridis(20))
+    plotGroupedHeatmap(example_sce, features=rownames(example_sce)[1:10], group="Group", zlim=c(0, 2))
+
+    # Works with rownames swapping.
+    plotGroupedHeatmap(example_sce, features = rowData(example_sce)[1:10, "ENS"], 
+        group="Group", swap_rownames = "ENS", columns = 1:20)
+})
