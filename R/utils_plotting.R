8376862a2940255986c5fd05abd75c02ae8ecc0d
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2022-09-19 09:37:03 +0100

    1.25.7; fix order_by argument #172

--- R/utils_plotting.R
+++ R/utils_plotting.R
@@ -68,7 +68,7 @@
     if (is.null(df$order_by)) {
         df$order_by <- seq_len(nrow(df))
     }
-    df <- df[df$order_by, ]
+    df <- df[order(df$order_by), ]
 
     for (o in other_fields) {
         other <- retrieveCellInfo(se, o, exprs_values=by_exprs_values,
