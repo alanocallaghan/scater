2791d108d972f8238edf7385154a27be3d7173f5
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2026-04-06 16:13:57 +0100

    Update R/reexports.R

--- R/reexports.R
+++ R/reexports.R
@@ -79,7 +79,7 @@ scuttle::sumCountsAcrossFeatures
 #' @export
 scuttle::uniquifyFeatureNames
 
-# Vendored from scuttle given that it's otherwise deprecated and we don't want to add a depedency on scrapper.
+# Vendored from scuttle given that it's otherwise deprecated and we don't want to add a dependency on scrapper.
 quick_means_by_group <- function(x, groups) {
     groups <- factor(groups)
     num.groups <- nlevels(groups)
